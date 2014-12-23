9fd042be3a333f71e1b59a40a004a49d1209b2a1
Jan Christoph Uhde <Jan@UhdeJC.com>
2014-12-23 18:22:59 +0100

    update function cache

--- Examples/function_cache.cpp
+++ Examples/function_cache.cpp
@@ -1,19 +1,34 @@
 #include <libobi/util/function_cache.hpp>
+#include <libobi/util/scoped_timer.hpp>
 #include <iostream>
+#include <future>
 
 using namespace std;
 using namespace obi::util;
 
-int foo(int a, int b){
-    return a+b;
+
+uint64_t fib(uint64_t n)
+{
+    if (n < 2)
+        return n;
+
+    future<uint64_t> n1 = async(fib, n - 1);
+    future<uint64_t> n2 = async(fib, n - 2);
+
+    return n1.get() + n2.get();
 }
 
 int main() {
-    std::function<decltype(foo)> foo_fn = &foo;
-    auto foo_cache = add_function_cache(foo_fn);
 
-    cout << foo_cache(1,2) << endl;
-    cout << foo_cache(1,2) << endl;
+    scoped_timer timer("function cache test");
+    function<decltype(fib)> fib_fn = &fib;
+    auto fib_cache = add_function_cache(fib_fn);
+
+    timer.add_step("init - done");
+    cout << fib_cache(8) << endl;
+    timer.add_step("without cache - done");
+    cout << fib_cache(8) << endl;
+    timer.add_step("with cache - done");
 
     return 0;
 }
