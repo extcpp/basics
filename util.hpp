e54a7f3ceff4553c7d35f7ade2ebbd2070ff1a97
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-04-01 11:35:33 +0200

    add: tracking class

--- util.hpp
+++ util.hpp
@@ -4,6 +4,7 @@
 #include "util/scoped_timer.hpp"
 #include "util/type_to_string.hpp"
 #include "util/function_cache.hpp"
+#include "util/tracking_class.hpp"
 
 #if __cpp_generic_lambdas >= 201304
     #include "util/functional.hpp"
---
+++ util/tracking_class.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+
+namespace obi { namespace util {
+
+    template<class T>
+    class tracking_class
+    {
+        T _value;
+    public:
+        tracking_class() : _value() {
+            std::cout << "Default Constructor:             " << _value << std::endl;
+        }
+        tracking_class(T v) : _value(v){
+            std::cout << "Constructor, value:              " << _value << std::endl;
+        }
+        tracking_class(const tracking_class& other) : _value(other._value){
+            std::cout << "Copy Constructor, value:         " << _value << std::endl;
+        }
+        tracking_class(tracking_class&& other) {
+            _value = std::move(other._value);
+            std::cout << "Move Constructor, value:         " << _value << std::endl;
+        }
+        ~tracking_class(){
+            std::cout << "Destructor, value:       " << _value << std::endl;
+        }
+        T set_value(T const value){
+            _value=value;
+        }
+    };
+}}  // namespace obi::util
