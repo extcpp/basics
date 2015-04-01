d4082b78542d95d4f2894b64bad77a684117ca63
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-04-01 11:38:11 +0200

    nochange

--- meta/has_member.hpp
+++ meta/has_member.hpp
@@ -9,7 +9,8 @@ namespace obi { namespace meta {
     struct has_value_var : std::false_type { };
 
     template<typename T>
-    struct has_value_var<T, decltype(std::declval<T>().value, void())> : std::true_type { };
+    //struct has_value_var<T, void_t<decltype(std::declval<T>().value)>> : std::true_type {};
+    struct has_value_var<T, decltype(std::declval<T>().value, void())> : std::true_type {};
 
 // has_type_member
     template<class, class = void>
@@ -17,6 +18,6 @@ namespace obi { namespace meta {
 
     template<class T>
     struct has_type_member<T, void_t<typename T::type> > : std::true_type {};
-
+    //struct has_type_member<T, decltype(typename T::type(),void())> : std::true_type {};
 
 }}  // namespace obi::meta
