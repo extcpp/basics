04e7da45b1472c8ff73298f91666b7f167c8b21d
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-07-13 22:07:54 +0200

    fix / finish simple2darray for usage with BGL

--- src/include/libobi/structures/simple2darray.hpp
+++ src/include/libobi/structures/simple2darray.hpp
@@ -2,41 +2,59 @@
 
 #include <vector>
 #include <type_traits>
+#include <utility>
 
 namespace obi { namespace structures {
-
 
 template <typename T>
 class array2d
 {
-    using data = std::vector<T>;
-    std::size_t _rows, _cols;
+    using data = typename std::vector<T>;
+    using size_type = typename data::size_type;
+    using diff_type = typename data::difference_type;
+
+    size_type _rows, _cols;
     data _data;
 
 public:
-    array2d(std::size_t rows, std::size_t cols): _rows(rows), _cols(cols){
+    array2d(size_type rows, size_type cols): _rows(rows), _cols(cols){
         _data.resize(rows * cols);
     }
 
     template<typename Iterator>
     class SubscriptionProxy{
-        typename data::iterator _begin;
+        Iterator _begin;
     public:
         SubscriptionProxy(Iterator begin) : _begin(begin) {};
-        T& operator[](std::size_t const j){
-            return *(_begin + j);
-        }
-        T const & operator[](std::size_t const j) const {
-            return *(_begin + j);
+        typename Iterator::reference operator[](std::size_t const j) const {
+            return *(_begin + static_cast<diff_type>(j));
         }
     };
 
-    auto operator[](std::size_t i){
-        return SubscriptionProxy<typename data::iterator>(_data.begin() + i * _cols);
+    auto operator[](size_type const i){
+        return SubscriptionProxy<typename data::iterator>(_data.begin() + static_cast<diff_type>(i * _cols));
     }
-    auto operator[](std::size_t i) const {
-        return SubscriptionProxy<typename data::const_iterator>(_data.begin() + i * _cols);
+    auto operator[](size_type const i) const {
+        return SubscriptionProxy<typename data::const_iterator>(_data.begin() + static_cast<diff_type>(i * _cols));
+    }
+
+    auto size() const {
+        return std::make_pair(_rows,_cols);
     }
 };
+
+template<typename T>
+std::ostream& operator <<(std::ostream& stream, obi::structures::array2d<T> const & arr){
+    using namespace std;
+    auto size = arr.size();
+    for(std::size_t j = 0; j < size.first; ++j){
+        for(std::size_t i = 0; i < size.second; ++i){
+            stream << arr[j][i] << " ";
+        }
+        stream << endl;
+    }
+    stream << endl;
+    return stream;
+}
 
 }}  // obi::structures
