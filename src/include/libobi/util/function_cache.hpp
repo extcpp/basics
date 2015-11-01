3d0158663089b445dfa788158b1145d61edcb50c
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-11-01 20:08:49 +0100

    add: some code to function cache - does not really work

--- src/include/libobi/util/function_cache.hpp
+++ src/include/libobi/util/function_cache.hpp
@@ -10,23 +10,40 @@ namespace obi { namespace util {
 
 // requires c++14
 // Function Pointer only
-auto add_function_cache = [](auto fun) {
+//
+// TODO - add parameter that limits the size of the cache
+//        this is difficult because we can have data of
+//        different functions in the same map and we are
+//        only able to distinguish by function pointer
+//        value
+//
+auto add_function_cache = [](auto fun, unsigned cache_size=1000000, double delete_percent=0.1) {
     using fun_type = decltype(fun);
     return ([=](auto... run_args) {
         using fun_return_type = std::result_of_t<fun_type(decltype(run_args)...)>;
+        static unsigned delete_on_overflow = static_cast<unsigned>(cache_size * delete_percent);
         static std::map<
             std::tuple<fun_type, decltype(run_args)...>,
             fun_return_type
         > result_cache;
+        static std::map<fun_type, unsigned> inserts;
         std::tuple<fun_type, decltype(run_args)...> tuple(fun, run_args...);
-        auto search = result_cache.find(tuple);
-        if ( search == result_cache.end()) {
+        auto result_search = result_cache.find(tuple);
+        if (result_search == result_cache.end()){
+            auto insert_num_search = inserts.find(fun);
+            if (insert_num_search == inserts.end()){
+                inserts[fun]=0;
+            } else {
+                insert_num_search->second++;
+                // TODO no overflow!!
+                // in case of overflow delete
+            }
             fun_return_type rv = fun(run_args...);
             result_cache[tuple] = rv;
             return rv;
         }
         else {
-            return search->second;
+            return result_search->second;
         }
     });
 };
