400e0eb2050d370cd6803602c1f60e6852b53c20
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-12-23 13:15:30 +0100

    change: improve readability

--- src/include/obi/structures/vector.hpp
+++ src/include/obi/structures/vector.hpp
@@ -4,15 +4,20 @@
 
 namespace obi { namespace structures {
 
+    // Will this work with 17?
+
     //custom types (via move)
     template <typename T, std::size_t N>
     class vector_base_user_defined {
     public:
         vector_base_user_defined() = default;
-        template <typename Arg, typename... Args, typename = std::enable_if_t<sizeof...(Args)+1 == N>>
+        //forwarding ctor
+        template <typename Arg, typename... Args
+                 ,typename = std::enable_if_t<sizeof...(Args)+1 == N> //check there are enough arguments
+                 >
         vector_base_user_defined(Arg&& arg, Args&&... args) : //at leaset one arg
-            _values {std::forward<Arg>(arg), std::forward<Args>(args)...}
-        {}
+            _values {std::forward<Arg>(arg), std::forward<Args>(args)...} {}
+
     private:
         std::array<T, N> _values;
     };
@@ -20,10 +25,12 @@ namespace obi { namespace structures {
     //build-in sse types etc
     template <typename T, std::size_t N, typename>
     class vector_base_arithmetic;
+
     template <typename T, std::size_t N, typename... Args>
     class vector_base_arithmetic<T, N, obi::meta::pack<Args...>> {
     public:
         vector_base_arithmetic() = default;
+        //copy ctor
         vector_base_arithmetic(Args... args) : _values{{args...}} {}
     private:
         std::array<T, N> _values;
@@ -31,13 +38,14 @@ namespace obi { namespace structures {
 
 
     template <typename T, std::size_t N>
-    class vector : std::conditional_t<std::is_arithmetic<T>::value,
-                                      vector_base_arithmetic<T, N, typename ::obi::meta::generate_pack<N, T>::type>,
-                                      vector_base_user_defined<T, N>>{
-
-        using base = std::conditional_t<std::is_arithmetic<T>::value,
-                                        vector_base_arithmetic<T, N, typename ::obi::meta::generate_pack<N, T>::type>,
-                                        vector_base_user_defined<T, N>
+    class vector :   std::conditional_t<std::is_arithmetic<T>::value
+                                       ,vector_base_arithmetic  <T, N, ::obi::meta::generate_pack_t<N, T>>
+                                       ,vector_base_user_defined<T, N>
+                                       >
+    {
+        using base = std::conditional_t<std::is_arithmetic<T>::value
+                                       ,vector_base_arithmetic  <T, N, ::obi::meta::generate_pack_t<N, T>>
+                                       ,vector_base_user_defined<T, N>
                                        >;
         using base::base;
     };
