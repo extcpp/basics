f7aaa1f785aa5e9f7da2c1cabcfdccd820371cb4
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-12-24 07:07:29 +0100

    add: some documenation

--- CMakeLib/obi_util.cmake
+++ CMakeLib/obi_util.cmake
@@ -1,3 +1,10 @@
+
+#! prefix string with provided symbol(s) until is has given length
+#
+#  in_string - sting to be prefixed
+#  length - desired length
+#  fill - symbols(s) used for filling
+#  out_string - this will hold the result
 function(prefix in_string length fill out_string)
     set(result "${in_string}")
     string(LENGTH "${in_string}" current_length)
@@ -10,6 +17,8 @@ function(prefix in_string length fill out_string)
     set("${out_string}" "${result}" PARENT_SCOPE)
 endfunction(prefix)
 
+#! this function acts like add_subdirectory but it checks
+#  if CMakeLists.txt exists in the directory before adding it
 function(add_subdirectory_checked dir debug)
     if(EXISTS "${dir}/CMakeLists.txt")
         add_subdirectory("${dir}")
