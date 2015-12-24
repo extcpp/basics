d986b1fbbfc43a9072f6f67964782ca2fcd9a560
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-12-24 08:01:16 +0100

    change: introduce naming schema for files in CMakeLib

---
+++ CMakeLib/README.rst
@@ -0,0 +1,10 @@
+CMake Library
+=============
+
+Naming of Files
+---------------
+
+Files prefixed with `obi_in_` just need to be included to have some impacked.
+On the other hand files prefixed with `obi_fm_` just define functions and
+macros that need to be executed in oder to have some effect.
+
--- CMakeLib/obi_fm_util.cmake
+++ CMakeLib/obi_fm_util.cmake
@@ -1,7 +1,7 @@
-
+#! some basic settings i use really often so lets have them in a macro
 macro(obi_settings)
-    include(obi_compiler_warnings)
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${stone-warnings}")
+    include(obi_in_compiler_warnings)
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${obi_stone-warnings}")
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR
        CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
         set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -ggdb -O0")
@@ -19,7 +19,7 @@ endmacro(obi_settings)
 #  length - desired length
 #  fill - symbols(s) used for filling
 #  out_string - this will hold the result
-function(prefix in_string length fill out_string)
+function(obi_prefix in_string length fill out_string)
     set(result "${in_string}")
     string(LENGTH "${in_string}" current_length)
 
@@ -29,11 +29,11 @@ function(prefix in_string length fill out_string)
     endwhile()
 
     set("${out_string}" "${result}" PARENT_SCOPE)
-endfunction(prefix)
+endfunction(obi_prefix)
 
 #! this function acts like add_subdirectory but it checks
 #  if CMakeLists.txt exists in the directory before adding it
-function(add_subdirectory_checked dir debug)
+function(obi_add_subdirectory dir debug)
     if(EXISTS "${dir}/CMakeLists.txt")
         add_subdirectory("${dir}")
         if(debug)
