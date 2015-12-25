0b9cfb6439c9609fd3be7d398e3a6185294a5a4a
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-12-25 08:22:28 +0100

    add: pragame once to algorithm

--- src/include/obi/algorithm.hpp
+++ src/include/obi/algorithm.hpp
@@ -1,2 +1,4 @@
 // Copyright - 2015 - Jan Christoph Uhde <Jan@UhdeJC.com>
+#pragma once
+
 #include "algorithm/basic.hpp"
--- src/include/obi/algorithm/basic.hpp
+++ src/include/obi/algorithm/basic.hpp
@@ -1,3 +1,6 @@
+// Copyright - 2015 - Jan Christoph Uhde <Jan@UhdeJC.com>
+#pragma once
+
 #include <iterator>
 #include <map>
 #include <stdexcept>
