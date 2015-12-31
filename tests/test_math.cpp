856e33e7200c260a74bfc5786019503c01e22745
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-12-31 15:52:30 +0100

    rename: testcase

--- tests/test_math.cpp
+++ tests/test_math.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <obi/math/primes.hpp>
 
-TEST(math_primes_find_nth, find_nth_prime){
+TEST(math_find_nth_prime, 10001){
     auto sieve = obi::math::create_sieve_of_eratosthenes(40000000);
     EXPECT_EQ(obi::math::find_nth_prime(10001, sieve).value
              ,std::size_t(104743)
