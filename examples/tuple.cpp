94489182a0ecda72e6dc8aad95bddb0d2f1f5360
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-15 00:46:49 +0100

    add: example for tuple

---
+++ examples/tuple.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <map>
+#include <iostream>
+#include <obi/util/show.hpp>
+#include <obi/meta/tuple.hpp>
+
+int main(int argc, const char *argv[]) {
+    // pull in operator
+    using obi::util::operator<<;
+
+    // define data
+    using combined = obi::meta::tuple_cat_t<std::tuple<int,float,void>
+                                          ,std::tuple<char,double>
+                                          >;
+
+    std::cout << obi::meta::tuple_size<combined>::value << std::endl;
+    return 0;
+}
+
