a8c1f4c0c632a247c029c65d4804c9cf92c5336c
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-18 07:54:42 +0100

    add: example for if_all_none

---
+++ examples/if_all_none.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <map>
+#include <iostream>
+#include <obi/util/show.hpp>
+#include <obi/meta/if_all_none.hpp>
+
+int main(int argc, const char *argv[]) {
+    // pull in operator
+    using obi::util::operator<<;
+
+
+    std::cout << std::boolalpha
+              << obi::meta::if_all<true,true>() << std::endl
+              << obi::meta::if_all_t<true,true>::value << std::endl
+              << obi::meta::if_all_v<true,true,false> << std::endl
+
+              << obi::meta::if_any<true,true>() << std::endl
+              << obi::meta::if_any_t<false,false>::value << std::endl
+              << obi::meta::if_any_v<true,false,true> << std::endl
+
+              << std::endl;
+    return 0;
+}
+
