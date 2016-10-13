9bfb13f29cd73f5fbb5d905f019b8fd0fcac3008
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-10-13 21:25:23 +0200

    move logger implementaion to cpp

--- include/obi/util/logging/functionality.hpp
+++ include/obi/util/logging/functionality.hpp
@@ -20,36 +20,8 @@ namespace obi { namespace  util { namespace logging {
             std::stringstream ss;
             logger(logtopic const& topic, level level_,
                    const char* file_name, int line_no,
-                   const char* function = "none"):ss() {
-                ss << "\n";
-                if (configuration::vim){
-                    ss << file_name << " +" << line_no << "\n";
-                }
-                ss << level_to_str(level_);
-                if(topic.id != topic::no_topic.id){
-                    ss << "(" << topic.name << ")";
-                }
-                if(configuration::filename){
-                    ss << " - " << basename(file_name) << ":" << line_no;
-                }
-                if(configuration::function){
-                    ss << " - " << function << "()";
-                }
-                ss << ": ";
-            }
-
-            ~logger(){
-                if(configuration::threads){
-                    // add pointer to global lockfree queue
-                    // so other thread releases and prints
-                    // free memory in same thread?!
-                    // _detail::add_queue(ss.str());
-                }
-                else{
-                    std::lock_guard<std::mutex> lock(logmutex);
-                    std::cerr << ss.rdbuf() << std::endl;
-                }
-            }
+                   const char* function = "none");
+            ~logger();
 
             template<typename T>
             void operator<< (T&& value){
@@ -64,8 +36,6 @@ namespace obi { namespace  util { namespace logging {
         for(auto& topic : _detail::topics_map){
             topic.second->trigger_level=level_;
         }
-
-
     }
 
 }}} // obi::util::logging
--- src/logging.cpp
+++ src/logging.cpp
@@ -12,4 +12,37 @@ namespace obi { namespace  util { namespace logging {
     bool configuration::filename{true};
     bool configuration::function{true};
     bool configuration::vim{true};
-}}}
+
+    _detail::logger::logger(logtopic const& topic, level level_,
+                            const char* file_name, int line_no,
+                            const char* function):ss() {
+        ss << "\n";
+        if (configuration::vim){
+            ss << "vim " << file_name << " +" << line_no << "\n";
+        }
+        ss << level_to_str(level_);
+        if(topic.id != topic::no_topic.id){
+            ss << "(" << topic.name << ")";
+        }
+        if(configuration::filename){
+            ss << " - " << basename(file_name) << ":" << line_no;
+        }
+        if(configuration::function){
+            ss << " - " << function << "()";
+        }
+        ss << ": ";
+    }
+
+    _detail::logger::~logger(){
+        if(configuration::threads){
+            // add pointer to global lockfree queue
+            // so other thread releases and prints
+            // free memory in same thread?!
+            // _detail::add_queue(ss.str());
+        }
+        else{
+            std::lock_guard<std::mutex> lock(logmutex);
+            std::cerr << ss.rdbuf() << std::endl;
+        }
+    }
+}}} // obi::util::logging
