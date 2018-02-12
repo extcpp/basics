e337cb17fd95df5778c4a9de45c97d82a7ae4399
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-02-12 23:41:10 +0100

    add more useful tests to result

--- tests/test_util_result.cpp
+++ tests/test_util_result.cpp
@@ -5,13 +5,19 @@
 #include <type_traits>
 
 namespace ou = obi::util;
+using namespace obi::util::tracing;
 
 TEST(util_result, lvalue_copy){
-    ou::tracing::all all;
-    ou::typed_result<ou::tracing::all> result{all};
-    bool is_same = std::is_same_v<ou::tracing::all
-                                 ,typename ou::typed_result<ou::tracing::all>::value_type
-                                 >;
-    ASSERT_TRUE(is_same);
+    all value;
+    ou::typed_result<all> result{value};
+    ASSERT_TRUE(result.value.copy_ctor);
+    ASSERT_TRUE((std::is_same_v<all, typename ou::typed_result<all>::value_type>));
+    ASSERT_TRUE((std::is_same_v<decltype(value), decltype(result.value)>));
 }
 
+TEST(util_result, rvalue){
+    ou::typed_result<all> result(all{});
+    ASSERT_TRUE(result.value.move_ctor);
+    ASSERT_TRUE((std::is_same_v<all, typename ou::typed_result<all>::value_type>));
+    result.value.print();
+}
