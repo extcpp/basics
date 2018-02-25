a0abe1249a23b1d5b78c7bb3736b38f70ccfee0f
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-02-25 22:22:55 +0100

    add comments

--- include/obi/config.hpp
+++ include/obi/config.hpp
@@ -1,9 +1,8 @@
 // Copyright - 2016 - Jan Christoph Uhde <Jan@UhdeJC.com>
 #pragma once
 
-//When using as header only lib
 #ifndef OBI_LINK_TO_LIB
-#   define OBI_INLINE inline
+#   define OBI_INLINE inline //When using as header only lib
 #else
 #   define OBI_INLINE
 #endif
