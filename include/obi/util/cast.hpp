80193640bf8da757eaad765eeff9c7b5c6b9cea6
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-05-19 10:07:18 +0200

    fix conversion functions

--- include/obi/util/cast.hpp
+++ include/obi/util/cast.hpp
@@ -41,112 +41,117 @@ to_signed_checked(T in){
 }
 
 
-    template <typename To, typename From>
-    std::enable_if_t<!std::is_array<To>::value,void>
-    convert(const From &from, To &to){
-        std::memcpy(&to, &from, sizeof(To));
-    }
+//// standard conversions - potentially different sizes
 
-    template <typename To, typename From, std::size_t length>
-    std::enable_if_t<std::is_array<To>::value,void>
-    convert(const From &from, To (&to)[length]){
-        std::memcpy(&to, &from, sizeof(To)*length);
-    }
+template <typename To, typename From>
+std::enable_if_t<!std::is_array<To>::value,void>
+convert(const From &from, To &to){
+    std::memcpy(&to, &from, sizeof(To));
+}
 
-    template <typename To, typename From>
-    inline To convert_checked(const From &from){
-        static_assert(sizeof(To) == sizeof(From)
-                     ,"type_cast requires types with equal size"
-                     );
-        To to;
-        std::memcpy(&to, &from, sizeof(To));
-        return to;
-    }
+template <typename To, typename From, std::size_t length>
+std::enable_if_t<std::is_array<To>::value,void>
+convert(const From &from, To (&to)[length]){
+    std::memcpy(&to, &from, sizeof(To)*length);
+}
 
-    template <typename To, typename From>
-    inline To convert_to_bigger(const From &from){
-        // copy `from` size form `from` to `to` and fill rest with 0
-        // FFFF      -> 0000 0000 => 0000 FFFF
+template <typename To, typename From> constexpr
+inline To convert_checked(const From &from){
+    static_assert(sizeof(To) == sizeof(From)
+                 ,"type_cast requires types with equal size"
+                 );
+    To to;
+    std::memcpy(&to, &from, sizeof(To));
+    return to;
+}
 
-        static_assert(sizeof(To) >= sizeof(From)
-                     ,"type_cast requires target with greater or equal size"
-                     );
+template <typename To, typename From> constexpr
+inline To convert_to_bigger(const From &from){
+    // copy `from` size form `from` to `to` and fill rest with 0
+    // FFFF      -> 0000 0000 => 0000 FFFF
 
-        To to = To(0);
-        std::memcpy(&to, &from, sizeof(From));
-        return to;
-    }
+    static_assert(sizeof(To) >= sizeof(From)
+                 ,"type_cast requires target with greater or equal size"
+                 );
 
-    template <typename To, typename From>
-    inline To convert_to_smaller(const From &from){
-        // copy `from` size form `from` to `to` and fill rest with 0
-        // 0000 FFFF -> 0000 => FFFF
+    To to = To(0);
+    std::memcpy(&to, &from, sizeof(From));
+    return to;
+}
 
-        static_assert(sizeof(To) <= sizeof(From)
-                     ,"type_cast requires target with less or equal size"
-                     );
+template <typename To, typename From> constexpr
+inline To convert_to_smaller(const From &from){
+    // copy `from` size form `from` to `to` and fill rest with 0
+    // 0000 FFFF -> 0000 => FFFF
 
-        To to;
-        std::memcpy(&to, &from, sizeof(To));
-        return to;
-    }
+    static_assert(sizeof(To) <= sizeof(From)
+                 ,"type_cast requires target with less or equal size"
+                 );
 
-    template <typename To, typename From>
-    inline To convert_different(const From &from){
-        // if `to` is less equal only copy `to` size form `from` to `to`
-        // otherwise copy `from` size form `from` to `to` and fill rest with 0
-        // 0000 FFFF ->      0000 =>      FFFF
-        // FFFF      -> 0000 0000 => 0000 FFFF
+    To to;
+    std::memcpy(&to, &from, sizeof(To));
+    return to;
+}
 
-        To to = To(0);
-        std::memcpy(&to, &from, (sizeof(To) <= sizeof(From)) ? sizeof(To) : sizeof(From));
-        return to;
-    }
+template <typename To, typename From> constexpr
+inline To convert_different(const From &from){
+    // if `to` is less equal only copy `to` size form `from` to `to`
+    // otherwise copy `from` size form `from` to `to` and fill rest with 0
+    // 0000 FFFF ->      0000 =>      FFFF
+    // FFFF      -> 0000 0000 => 0000 FFFF
 
-    bool d = std::is_enum_v<int>;
-    // enum conversion
-	namespace _detail {
-        //use underlying type if Type is a enum
-		template <typename E, typename U = std::underlying_type_t<E>>
-		using enable_if_enum_t = std::enable_if_t<std::is_enum_v<E>, U>;
-
-		template <typename E1, typename E2>
-		constexpr bool is_enums_v = std::is_enum_v<E1> && std::is_enum_v<E2>;
-
-		template <typename E1, typename E2>
-        constexpr bool is_same_underlying_v = std::is_same_v<std::underlying_type_t<E1>,std::underlying_type_t<E1>>;
-	}
+    To to = To(0);
+    std::memcpy(&to, &from, (sizeof(To) <= sizeof(From)) ? sizeof(To) : sizeof(From));
+    return to;
+}
 
 
-	template <typename T, typename Enum, typename U = std::underlying_type_t<Enum>>
-    inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<T,U>>
-	enum_to_underlying(Enum e) noexcept {
-		return static_cast<U>(e);
-	}
+//// enum - conversion
 
-    template <typename Enum, typename U = std::underlying_type_t<Enum>>
-    inline constexpr std::enable_if_t<std::is_enum_v<Enum>>
-	enum_to_underlying_unsafe(Enum e) noexcept {
-		return static_cast<U>(e);
-	}
+namespace _detail {
+    //use underlying type if Type is a enum
+	template <typename E, typename U = std::underlying_type_t<E>>
+	using enable_if_enum_t = std::enable_if_t<std::is_enum_v<E>, U>;
 
-	template <typename Enum, typename T, typename U = std::underlying_type_t<Enum>>
-	inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<U,T>, Enum>
-	underlying_to_enum(T value) noexcept {
-		return static_cast<Enum>(value);
-	}
+	template <typename E1, typename E2>
+	constexpr bool is_enums_v = std::is_enum_v<E1> && std::is_enum_v<E2>;
 
-	template <typename EnumOut, typename EnumIn>
-	inline constexpr std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut>, EnumOut>
-	enum_to_enum_unsafe(EnumIn e) noexcept {
-		return underlying_to_enum<EnumOut>(enum_to_underlying(e));
-	}
+	template <typename E1, typename E2>
+    constexpr bool is_same_underlying_v = std::is_same_v<std::underlying_type_t<E1>,std::underlying_type_t<E1>>;
+}
 
-	template <typename EnumOut, typename EnumIn>
-	inline constexpr std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut> &&
-                                      _detail::is_same_underlying_v<EnumIn,EnumOut>, EnumOut>
-	enum_to_enum(EnumIn e) noexcept {
-		return underlying_to_enum<EnumOut>(enum_to_underlying(e));
-	}
+/// to underlying
+template <typename T, typename Enum, typename U = std::underlying_type_t<Enum>>
+inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<T,U>, T>
+enum_to_underlying(Enum e) noexcept {
+	return static_cast<U>(e);
+}
+
+template <typename Enum, typename U = std::underlying_type_t<Enum>>
+inline constexpr _detail::enable_if_enum_t<Enum,U>
+enum_to_underlying_unsafe(Enum e) noexcept {
+	return static_cast<U>(e);
+}
+
+/// to enum
+template <typename Enum, typename T, typename U = std::underlying_type_t<Enum>>
+inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<U,T>, Enum>
+underlying_to_enum(T value) noexcept {
+	return static_cast<Enum>(value);
+}
+
+/// enum to enum
+template <typename EnumOut, typename EnumIn>
+inline constexpr std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut>, EnumOut>
+enum_to_enum_unsafe(EnumIn e) noexcept {
+	return underlying_to_enum<EnumOut>(enum_to_underlying_unsafe(e));
+}
+
+template <typename EnumOut, typename EnumIn>
+inline constexpr std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut> &&
+                                  _detail::is_same_underlying_v<EnumIn,EnumOut>, EnumOut>
+enum_to_enum(EnumIn e) noexcept {
+	return underlying_to_enum<EnumOut>(enum_to_underlying_unsafe(e));
+}
 
 }} // obi::util
--- tests/util_cast.cpp
+++ tests/util_cast.cpp
@@ -2,21 +2,31 @@
 #include <obi/util/cast.hpp>
 
 TEST(util_cast, enum_to_underlying){
-    enum class enum_a{
+    enum class enum_a : int {
         one = 1,
         two = 2,
         three = 3
     };
-    ASSERT_EQ(obi::util::enum_to_underlying(enum_a::one), 1);
+
+    ASSERT_EQ(obi::util::enum_to_underlying_unsafe(enum_a::one), 1);
+    ASSERT_EQ(obi::util::enum_to_underlying<int>(enum_a::one), 1);
 }
 
 TEST(util_cast, underlying_to_enum){
-    enum class enum_a{
+    enum class enum_a : int {
         one = 1,
         two = 2,
         three = 3
     };
+
+    enum class enum_b : long {
+        one = 1,
+        two = 2,
+        three = 3
+    };
+
     ASSERT_EQ(obi::util::underlying_to_enum<enum_a>(2), enum_a::two);
+    ASSERT_EQ(obi::util::underlying_to_enum<enum_b>(2L), enum_b::two);
 }
 
 TEST(util_cast, enum_to_enum){
@@ -25,16 +35,18 @@ TEST(util_cast, enum_to_enum){
         two = 2,
         three = 3
     };
+
     enum class enum_b : int {
         one = 1,
         two = 2,
         three = 3
     };
+
     enum class enum_c : long {
         one = 1,
         two = 2,
         three = 3
     };
+
     ASSERT_EQ(obi::util::enum_to_enum<enum_b>(enum_a::three), enum_b::three);
-    //obi::util::enum_to_enum<enum_c>(enum_a::three), enum_c::three); fails to compile
 }
