6446697f46d0cec1da7127abf201c62c4af9dc8c
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-05-20 15:28:33 +0200

    add test for size_of

--- tests/util_cast.cpp
+++ tests/util_cast.cpp
@@ -1,6 +1,15 @@
 #include <gtest/gtest.h>
 #include <obi/util/cast.hpp>
 
+namespace ou = ::obi::util;
+
+TEST(util_cast, size_of){
+    std::size_t obi = ou::size_of<std::uint64_t,std::uint32_t>();
+    std::size_t norm = sizeof(std::uint64_t) + sizeof(std::uint32_t);
+    ASSERT_EQ(obi, norm);
+    ASSERT_EQ(obi, 12);
+}
+
 TEST(util_cast, enum_to_underlying){
     enum class enum_a : int {
         one = 1,
@@ -8,8 +17,8 @@ TEST(util_cast, enum_to_underlying){
         three = 3
     };
 
-    ASSERT_EQ(obi::util::enum_to_underlying_unsafe(enum_a::one), 1);
-    ASSERT_EQ(obi::util::enum_to_underlying<int>(enum_a::one), 1);
+    ASSERT_EQ(ou::enum_to_underlying_unsafe(enum_a::one), 1);
+    ASSERT_EQ(ou::enum_to_underlying<int>(enum_a::one), 1);
 }
 
 TEST(util_cast, underlying_to_enum){
@@ -25,8 +34,8 @@ TEST(util_cast, underlying_to_enum){
         three = 3
     };
 
-    ASSERT_EQ(obi::util::underlying_to_enum<enum_a>(2), enum_a::two);
-    ASSERT_EQ(obi::util::underlying_to_enum<enum_b>(2L), enum_b::two);
+    ASSERT_EQ(ou::underlying_to_enum<enum_a>(2), enum_a::two);
+    ASSERT_EQ(ou::underlying_to_enum<enum_b>(2L), enum_b::two);
 }
 
 TEST(util_cast, enum_to_enum){
@@ -48,5 +57,5 @@ TEST(util_cast, enum_to_enum){
         three = 3
     };
 
-    ASSERT_EQ(obi::util::enum_to_enum<enum_b>(enum_a::three), enum_b::three);
+    ASSERT_EQ(ou::enum_to_enum<enum_b>(enum_a::three), enum_b::three);
 }
