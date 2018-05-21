625ea95d24a3d105f564ef2b9ee14350afc9d0bd
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-05-21 17:01:50 +0200

    add de-serialization

--- include/obi/util/serialization.hpp
+++ include/obi/util/serialization.hpp
@@ -4,6 +4,24 @@
 
 namespace obi { namespace util {
 
+char* to_char_ptr(std::byte* in){
+    return reinterpret_cast<char*>(in);
+}
+
+char const* to_char_ptr(std::byte const* in){
+    return reinterpret_cast<char const*>(in);
+}
+
+template<std::size_t N>
+char* to_char_ptr(std::array<std::byte,N>& arr){
+    return reinterpret_cast<char*>(arr.data());
+}
+
+template<std::size_t N>
+char const* to_char_ptr(std::array<std::byte,N> const& arr){
+    return reinterpret_cast<char const*>(arr.data());
+}
+
 //// to little storage
 // with string
 template <typename T> //constexpr
@@ -20,8 +38,8 @@ to_little_storage(T in, std::string& out) {
 // with pointer - does not advance pointer
 // returns advanced pointer position
 template <typename T> //constexpr
-std::enable_if_t<std::is_integral_v<T>, char*>
-to_little_storage(T in, char* out) {
+std::enable_if_t<std::is_integral_v<T>, std::byte*>
+to_little_storage(T in, std::byte* out) {
     in = endian::host_to_little(in);
     std::memcpy(out, &in, sizeof(std::decay_t<T>));
     return out + sizeof(std::decay_t<T>);
@@ -30,64 +48,100 @@ to_little_storage(T in, char* out) {
 // with pointer ref - advances cursor
 template <typename T> //constexpr
     std::enable_if_t<std::is_integral_v<T>, void>
-    to_little_storage_advance(T in, char*& out) {
+    to_little_storage_advance(T in, std::byte*& out) {
         in = endian::host_to_little(in);
         std::memcpy(out, &in, sizeof(std::decay_t<T>));
         out += sizeof(std::decay_t<T>);
 }
 
-
 // writes multiple arguments into an array
 template <typename ...T, std::size_t N = size_of<T...>()> //constexpr
 std::array<std::byte, N>
-to_little_storage_array(T& ...args){
+to_little_storage(T& ...args){
     std::array<std::byte,N> result;
-    char* cursor = &result;
+    std::byte* cursor = result.data();
     (to_little_storage_advance(args,cursor), ...);
     return result;
 }
+//// to little storage - end
 
 //// to big storage
+// with pointer - does not advance pointer
+// returns advanced pointer position
 template <typename T> //constexpr
-std::enable_if_t<std::is_integral_v<T>, char*>
-to_big_storage(T in, char* out) {
+std::enable_if_t<std::is_integral_v<T>, std::byte*>
+to_big_storage(T in, std::byte* out) {
     in = endian::host_to_big(in);
     std::memcpy(out, &in, sizeof(std::decay_t<T>));
     return out + sizeof(std::decay_t<T>);
 }
 
-//// from little storage
+// with pointer ref - advances cursor
 template <typename T> //constexpr
-std::enable_if_t<std::is_integral_v<T>,char const*>
-from_little_storage(char const* in, T& out){
+    std::enable_if_t<std::is_integral_v<T>, void>
+    to_big_storage_advance(T in, std::byte*& out) {
+        in = endian::host_to_big(in);
+        std::memcpy(out, &in, sizeof(std::decay_t<T>));
+        out += sizeof(std::decay_t<T>);
+}
+
+// writes multiple arguments into an array
+template <typename ...T, std::size_t N = size_of<T...>()> //constexpr
+std::array<std::byte, N>
+to_big_storage(T& ...args){
+    std::array<std::byte,N> result;
+    std::byte* cursor = result.data();
+    (to_big_storage_advance(args,cursor), ...);
+    return result;
+}
+//// to big storage - end
+
+//// from little storage
+// with pointer - does not advance pointer
+// returns advanced pointer position
+template <typename T> //constexpr
+std::enable_if_t<std::is_integral_v<T>,std::byte const*>
+from_little_storage(std::byte const* in, T& out){
     std::memcpy(&out, in, sizeof(std::decay_t<T>));
     out = endian::little_to_host(out);
     return in + sizeof(std::decay_t<T>);
 }
 
+// with pointer ref - advances cursor
 template <typename T> //constexpr
-std::enable_if_t<std::is_integral_v<T>,std::decay_t<T>>
-from_little_storage_advance(char const*& in){
-    std::decay_t<T> out;
+std::enable_if_t<std::is_integral_v<T>,void>
+from_little_storage_advance(std::byte const*& in, T& out){
     in = from_little_storage(in,out);
-    return out;
 }
 
+template <typename ...T> //constexpr
+void from_little_storage(std::byte const* in, T& ...outs){
+    (from_little_storage_advance(in,outs), ...);
+}
+//// from little storage - end
+
 //// from big storage
+// with pointer - does not advance pointer
+// returns advanced pointer position
 template <typename T> //constexpr
-std::enable_if_t<std::is_integral_v<T>,char const*>
-from_big_storage(char const*& in, T& out){
+std::enable_if_t<std::is_integral_v<T>,std::byte const*>
+from_big_storage(std::byte const*& in, T& out){
     std::memcpy(&out, in, sizeof(std::decay_t<T>));
     out = endian::big_to_host(out);
     return in + sizeof(std::decay_t<T>);
 }
 
+// with pointer ref - advances cursor
 template <typename T> //constexpr
-std::enable_if_t<std::is_integral_v<T>,std::decay_t<T>>
-from_big_storage_advance(char*& in){
-    std::decay_t<T> out;
+std::enable_if_t<std::is_integral_v<T>,void>
+from_big_storage_advance(std::byte*& in,T& out){
     in = from_big_storage(in,out);
-    return out;
 }
+
+template <typename ...T> //constexpr
+void from_big_storage(std::byte const* in, T& ...outs){
+    (from_big_storage_advance(in,outs), ...);
+}
+//// from big storage - end
 
 }} // namespace obi::util
--- tests/util_serialization.cpp
+++ tests/util_serialization.cpp
@@ -8,10 +8,10 @@ using namespace ::obi::util;
 
 #ifdef OBI_LITTLE_ENDIAN
 
-TEST(util_serialization, little_storage){
+TEST(util_serialization, little_storage_integrals){
     std::uint32_t num = 0x01020304U;
 
-    std::array<char,sizeof(num)> arr;
+    std::array<std::byte,sizeof(num)> arr;
     to_little_storage(num, &arr[0]); // pointer
 
     std::string str;
@@ -24,7 +24,7 @@ TEST(util_serialization, little_storage){
     }
 
     {   // cursor advancing method
-        char* cursor = &arr[0];
+        std::byte* cursor = &arr[0];
         to_little_storage_advance(num, cursor);
         auto rv = std::memcmp(&arr[0], str.data(), sizeof(num));
         ASSERT_EQ(rv,0);
@@ -33,18 +33,45 @@ TEST(util_serialization, little_storage){
 
     {   // not cursor advancing method
         std::uint32_t num_out = 0;
-        from_little_storage(str.data(), num_out);
+        from_little_storage((std::byte*)str.data(), num_out);
         ASSERT_EQ(num, num_out);
     }
 
     {   // cursor advancing method
-        char const* cursor = str.data();
-        std::uint32_t num_out = from_little_storage_advance<std::uint32_t>(cursor);
+        std::byte const* cursor = (std::byte*)str.data();
+        std::uint32_t num_out;
+        from_little_storage_advance(cursor,num_out);
         ASSERT_EQ(num, num_out);
-        ASSERT_EQ(cursor ,str.data()+sizeof(std::uint32_t));
+        ASSERT_EQ(cursor ,(std::byte*)str.data()+sizeof(std::uint32_t));
     }
 }
 
+TEST(util_serialization, little_storage_multi){
+    std::uint64_t a_in = 42;
+    std::uint32_t b_in = 23;
+    auto array = to_little_storage(a_in, b_in);
+    ASSERT_EQ(array.size(), size_of(a_in, b_in));
+
+
+    {
+        std::uint64_t a_out = 0;
+        std::uint32_t b_out = 0;
+        std::byte const* cursor = array.data();
+        from_little_storage_advance(cursor, a_out);
+        from_little_storage_advance(cursor, b_out);
+        ASSERT_EQ(a_in, a_out);
+        ASSERT_EQ(b_in, b_out);
+    }
+
+    {
+        std::uint64_t a_out = 0;
+        std::uint32_t b_out = 0;
+        std::byte const* cursor = array.data();
+        from_little_storage(cursor, a_out, b_out);
+        ASSERT_EQ(a_in, a_out);
+        ASSERT_EQ(b_in, b_out);
+    }
+}
 
 #elif OBI_BIG_ENDIAN
 #else
