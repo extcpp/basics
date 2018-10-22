d41a17a4c6473220cd7edbfe86a998990b252b59
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-10-22 20:23:39 +0200

    add example for serialize

---
+++ examples/serialize.cpp
@@ -0,0 +1,9 @@
+#include <obi/util/serialization.hpp>
+
+int main(/*int argc, const char *argv[]*/) {
+    std::uint64_t a_in = 42;
+    auto volatile array = obi::util::to_big_storage(a_in);
+    (void) array;
+    return 0;
+}
+
