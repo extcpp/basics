a9d8bd5eb71de1f389230a65af46c96f099cbe70
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-10-28 19:54:31 +0100

    add points

--- include/obi/math/geometry/simple_shapes.hpp
+++ include/obi/math/geometry/simple_shapes.hpp
@@ -7,30 +7,71 @@
 
 namespace obi { namespace math { namespace geometry {
 
-    template <typename T, int N>
+
+    template <typename T, std::size_t N>
     struct point {
         std::array<T,N> data;
 
-        template<typename return_type = T>
-        std::enable_if_t< (N>0), return_type >
-        get_x() const {
+        point(point const&) = default;
+
+        template <typename ...Forward
+                 ,typename = std::enable_if_t<!(std::is_same_v<point<T,N>,std::decay_t<Forward>> && ...)>
+                 >
+        point(Forward&& ...f): data{std::forward<Forward>(f)...} {}
+
+        T& get_x() {
+            static_assert(N > 0);
             return this->data[0];
         }
 
-        template<typename return_type = T>
-        std::enable_if_t< (N>1), return_type >
-        get_y() const {
+        T& get_y() {
+            static_assert(N > 1);
             return this->data[1];
         }
 
-        template<typename return_type = T>
-        std::enable_if_t< (N>2), return_type >
-        get_z() const {
+        T& get_z() {
+            static_assert(N > 2);
             return this->data[2];
+        }
+
+        T& get_x() const {
+            static_assert(N > 0);
+            return this->data[0];
+        }
+
+        T& get_y() const {
+            static_assert(N > 1);
+            return this->data[1];
+        }
+
+        T& get_z() const {
+            static_assert(N > 2);
+            return this->data[2];
+        }
+
+        T& operator[](std::size_t index){
+            return data[index];
+        }
+
+        T const& operator[](std::size_t index) const {
+            return data[index];
+        }
+
+        std::size_t size() const {
+            return data.size();
         }
     };
 
-    template <typename T, int N, bool validate = false>
+    template <typename T, std::size_t N>
+    point<T, N> operator-(point<T,N> const& left, point<T,N> const& right) {
+        auto rv = left;
+        for(std::size_t i = 0; i < N; ++i){
+            rv[i]-=right[i];
+        }
+        return rv;
+    }
+
+    template <typename T, std::size_t N, bool validate = false>
     struct rectangle {
         point<T,N> smaller;
         point<T,N> greater;
@@ -51,10 +92,11 @@ namespace obi { namespace math { namespace geometry {
         }
 
         rectangle(point<T,N> a, point<T,N> b) noexcept(!validate)
-            : smaller(std::move(a)), greater(std::move(b)) {
+            : smaller(a), greater(b) {
             if constexpr (validate) {
                 auto validation_result = validate_points(this->smaller, this->greater);
                 if (validation_result == points_valid::NO){
+                    throw std::logic_error("");
                     throw std::invalid_argument("point<T,N>s provided do not form a rectangle");
                 } else if (validation_result == points_valid::SWITCH) {
                     auto tmp = std::move(smaller);
@@ -62,6 +104,10 @@ namespace obi { namespace math { namespace geometry {
                     greater = std::move(tmp);
                 }
             }
+        }
+
+        point<T,N> size(){
+            return greater - smaller;
         }
     };
 
