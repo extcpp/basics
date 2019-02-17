7b6fb925e4c23d6478a93fa62bb7e8dda7633c02
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-02-17 10:34:30 +0100

    add code to access private members

---
+++ include/obi/util/access_private.hpp
@@ -0,0 +1,33 @@
+// Copyright - 2019 - Jan Christoph Uhde <Jan@UhdeJC.com>
+#pragma once
+
+template <typename type>
+struct access {
+    inline static type member_pointer;
+};
+
+template<typename type, type pointer>
+struct create_access : access<type> {
+    struct assign_pointer {
+        assign_pointer() { access<type>::member_pointer = pointer; }
+    };
+    inline static assign_pointer by_calling_default_ctor{};
+};
+
+// Usage:
+//
+// #include <iostream>
+// struct Alexander {
+// private:
+//     void quote() {
+//          std::cout << "Performance constraints are good for creativity." << std::endl;
+//     }
+// };
+//
+// using type = void(Alexander::*)();
+// template struct create_access<type, &Alexander::quote>;
+//
+// int main() {
+//     Alexander s;
+//     (s.*access<type>::member_pointer)();
+// }
