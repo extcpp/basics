d310b04c9ba2730d0d6c6c9115d97fe85a8e1126
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-05-02 17:33:54 +0200

    improve macros

--- include/obi/macros/general.hpp
+++ include/obi/macros/general.hpp
@@ -4,11 +4,20 @@
 #define OBI_MACROS_GENERAL_HEADER
 
 #define OBI_NO_IMPL "NOT IMPLEMENTED NOW PLEASE FAIL HERE"
-#define OBI_NOTHING
-#define OBI_STRINGIFY(a) #a
-#define OBI_CONCATENATE_IMPL(s1, s2) s1##s2
-#define OBI_CONCATENATE(s1, s2) OBI_CONCATENATE_IMPL(s1, s2)
+#define OBI_NOTHING()
 
+#define OBI_DELETE(...)
+#define OBI_EXPAND(...) __VA_ARGS__
+
+#define OBI_GET2ND(first, second, ...) second
+// returns 0 is there is only one arg. return 2 arg otherwise
+#define _OBI_ZERO_IF_ONE_ARG(...) OBI_GET2ND(__VA_ARGS__, 0,)
+
+#define OBI_STRINGIFY(a) #a
+
+// use indirection (_OBI_CONCATENATE_IMPL) because ## inhibits expansion
+#define _OBI_CONCATENATE_IMPL(s1, ...) s1 ## __VA_ARGS__
+#define OBI_CONCATENATE(s1, ...) _OBI_CONCATENATE_IMPL(s1, __VA_ARGS__)
 
 // __COUNTER__ provides increasing non negative number
 #ifdef __COUNTER__
