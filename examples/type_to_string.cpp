064bea20fb9664c17ca735282c2a6f23d67e56b7
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-05-05 20:00:54 +0200

    exclude example from builds with VisualStudio pre 2019

--- examples/type_to_string.cpp
+++ examples/type_to_string.cpp
@@ -1,5 +1,6 @@
 // Copyright - xxxx-2019 - Jan Christoph Uhde <Jan@UhdeJC.com>
 #include <obi/util/type_to_string.hpp>
+#include <obi/macros/platform.hpp>
 
 using namespace obi::util;
 
@@ -10,10 +11,12 @@ int main()
     const std::string s("foo");
     const double& dr = d;
 
+#if !defined(OBI_WINDOWS) || _MSC_VER >= 1920
     OBI_TYPE_PRINT(i);
     OBI_TYPE_PRINT(d);
     OBI_TYPE_PRINT(dr);
     OBI_TYPE_PRINT(s);
+#endif
 
     return 0;
 }
