081e7922b84cf2219e2dc5c55ec4c0e5be4f69c2
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-05-11 18:36:33 +0200

    add to benchmark header

--- include/obi/util/benchmark.h
+++ include/obi/util/benchmark.h
@@ -3,10 +3,14 @@
 #ifndef OBI_UTIL_BENCHMARK_HEADER
 #define OBI_UTIL_BENCHMARK_HEADER
 
+#include <obi/macros/compiler.hpp>
+#include <atomic>
+
 namespace obi { namespace util {
 
-inline void escape(void* pointer) {
-    // https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#Extended-Asm
+inline void do_not_optimize(void* pointer) {
+#ifdef OBI_COMPILER_GNU
+    // Ihttps://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#Extended-Asm
     asm volatile(""             // template
                 :               // output
                 : "g"(pointer)  // input - "g" any general register
@@ -14,6 +18,25 @@ inline void escape(void* pointer) {
                                 // prevent the processor from doing speculative
                                 // reads past the asm statement. To prevent that
                                 // you need processor-specific fence instructions.
+#else
+    // https://docs.microsoft.com/en-us/cpp/intrinsics/readwritebarrier?view=vs-2019
+    // _ReadWriteBarrier
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+#endif
+}
+
+inline void clobber_memory() {
+#ifdef OBI_COMPILER_GNU
+    asm volatile(""             // template
+                :               // output
+                :
+                : "memory");    // clobber - Note that this clobber does not
+                                // prevent the processor from doing speculative
+                                // reads past the asm statement. To prevent that
+                                // you need processor-specific fence instructions.
+#else
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+#endif
 }
 
 }}
