16e4ff11e96359dbe78a425ed462f91f4dc03e3c
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-07-12 21:13:29 +0200

    fix matrix and and add operator<<

--- include/ext/math/math_types.hpp
+++ include/ext/math/math_types.hpp
@@ -6,7 +6,7 @@
 #    include <vector>
 
 #    include <ext/macros/assert.hpp>
-
+#    include <ostream>
 namespace ext { namespace math {
 
 template<typename T, std::size_t N>
@@ -93,102 +93,127 @@ vec<T, N> operator+(vec<T, N> const& left, vec<T, N> const& right) {
 
 namespace _detail {
 
-template<typename Container, typename T, bool i_linear, bool checked>
-constexpr T const& matrix_get(Container const& cont,
-                              std::size_t const& column_count,
-                              std::size_t const& row_count,
-                              std::size_t i,
-                              std::size_t j) {
+template<typename Container, bool row_major, bool checked>
+constexpr auto const& matrix_get(Container const& cont,
+                                 std::size_t const& column_count,
+                                 std::size_t const& row_count,
+                                 std::size_t i,
+                                 std::size_t j) {
     if constexpr (checked) {
-        if (i >= column_count || j >= row_count) {
+        if (i >= row_count || j >= column_count) {
             throw std::runtime_error("out of range");
         }
     }
 
-    if constexpr (i_linear) {
-        return cont.data[i + column_count * j];
+    if constexpr (row_major) {
+        return cont.data[j + column_count * i];
     } else {
-        return cont.data[j + row_count * i];
+        return cont.data[i + row_count * j];
     }
 }
 
-template<typename Container, typename T, bool i_linear, bool checked>
-constexpr T const& matrix_set(Container& cont,
-                              std::size_t const& column_count,
-                              std::size_t const& row_count,
-                              std::size_t i,
-                              std::size_t j,
-                              T&& value) {
+template<typename Container, bool row_major, bool checked, typename T>
+constexpr auto const& matrix_set(Container& cont,
+                                 std::size_t const& column_count,
+                                 std::size_t const& row_count,
+                                 std::size_t i,
+                                 std::size_t j,
+                                 T&& value) {
     if constexpr (checked) {
-        if (i >= column_count || j >= row_count) {
+        if (i >= row_count || j >= column_count) {
             throw std::runtime_error("out of range");
         }
     }
 
-    if constexpr (i_linear) {
-        return cont.data[i + column_count * j] = std::forward<T>(value);
+    if constexpr (row_major) {
+        return cont.data[j + column_count * i] = std::forward<T>(value);
     } else {
-        return cont.data[j + row_count * i] = std::forward<T>(value);
+        return cont.data[i + row_count * j] = std::forward<T>(value);
     }
 }
 
 } // namespace _detail
 
+// https://simple.wikipedia.org/wiki/Matrix_(mathematics)#/media/File:Matrix.svg
 
-template<typename T, bool i_linear = true, bool checked = false>
+
+template<typename T, bool row_major = true, bool checked = false>
 struct dynamic_matrix {
     using value_type = T;
     std::size_t const row_count;    // j
     std::size_t const column_count; // i
     std::vector<T> data;
 
-    dynamic_matrix(std::size_t m = 1, std::size_t n = 1) : row_count(m), column_count(n) {}
+    dynamic_matrix(std::size_t m = 1, std::size_t n = 1) : row_count(m), column_count(n), data(n * m) {}
 
     void init_all_to(value_type const& value) {
-        std::fill(data.begin, data.end(), value);
+        std::fill(data.begin(), data.end(), value);
     }
 
     T const& get(std::size_t i, std::size_t j) const {
-        return _detail::matrix_get<dynamic_matrix, T, i_linear, checked>(*this, column_count, row_count, i, j);
+        return _detail::matrix_get<dynamic_matrix, row_major, checked>(*this, column_count, row_count, i, j);
     }
 
     T const& set(std::size_t i, std::size_t j, T const& value) {
-        return _detail::matrix_set<dynamic_matrix, T, i_linear, checked>(*this, column_count, row_count, i, j, value);
+        return _detail::matrix_set<dynamic_matrix, row_major, checked>(*this, column_count, row_count, i, j, value);
     }
 
     T const& set(std::size_t i, std::size_t j, T&& value) {
-        return _detail::matrix_set<dynamic_matrix, T, i_linear, checked>(
+        return _detail::matrix_set<dynamic_matrix, row_major, checked>(
             *this, column_count, row_count, i, j, std::move(value));
     }
 };
 
 
 // M rows - N cloumns
-template<typename T, std::size_t M, std::size_t N, bool i_linear = true, bool checked = false>
+template<typename T, std::size_t M, std::size_t N, bool row_major = true, bool checked = false>
 struct static_matrix {
     using value_type = T;
-    constexpr static std::size_t column_count = N; // i
-    constexpr static std::size_t row_count = M;    // j
+    constexpr static std::size_t column_count = N; // j
+    constexpr static std::size_t row_count = M;    // i
 
     std::array<T, row_count * column_count> data;
 
     void init_all_to(value_type const& value) {
-        std::fill(data.begin, data.end(), value);
+        std::fill(data.begin(), data.end(), value);
     }
 
     T const& get(std::size_t i, std::size_t j) const {
-        return _detail::matrix_get<static_matrix, T, i_linear, checked>(*this, column_count, row_count, i, j);
+        return _detail::matrix_get<static_matrix, row_major, checked>(*this, column_count, row_count, i, j);
     }
 
     T const& set(std::size_t i, std::size_t j, T const& value) {
-        return _detail::matrix_set<static_matrix, T, i_linear, checked>(*this, column_count, row_count, i, j, value);
+        return _detail::matrix_set<static_matrix, row_major, checked>(*this, column_count, row_count, i, j, value);
     }
 
     T const& set(std::size_t i, std::size_t j, T&& value) {
-        return _detail::matrix_set<static_matrix, T, i_linear, checked>(
+        return _detail::matrix_set<static_matrix, row_major, checked>(
             *this, column_count, row_count, i, j, std::move(value));
     }
 };
+
+namespace _detail {
+template<typename T>
+inline std::ostream& to_stream(std::ostream& os, T const& matrix) {
+    for (std::size_t j = 0; j < matrix.row_count; j++) {
+        for (std::size_t i = 0; i < matrix.column_count; i++) {
+            os << matrix.get(i, j) << " ";
+        }
+        os << "\n";
+    }
+    return os;
+}
+} // namespace _detail
+
+template<typename T, bool row_major, bool checked>
+inline std::ostream& operator<<(std::ostream& os, dynamic_matrix<T, row_major, checked> const& matrix) {
+    return _detail::to_stream(os, matrix);
+}
+
+template<typename T, std::size_t M, std::size_t N, bool row_major, bool checked>
+inline std::ostream& operator<<(std::ostream& os, static_matrix<T, M, N, row_major, checked> const& matrix) {
+    return _detail::to_stream(os, matrix);
+}
 
 }} // namespace ext::math
 #endif
