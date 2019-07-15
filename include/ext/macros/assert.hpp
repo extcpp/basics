ed625be66d7fde64b95180b24a1bf7e75d953ab6
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-07-15 17:30:35 +0200

    improve assert macro

--- include/ext/macros/assert.hpp
+++ include/ext/macros/assert.hpp
@@ -1,14 +1,19 @@
 // Copyright - xxxx-2019 - Jan Christoph Uhde <Jan@UhdeJC.com>
 #pragma once
+
+#include <ext/macros/compiler.hpp>
+#include <iostream>
+
 #ifndef EXT_MACROS_ASSERT_HEADER
 #    define EXT_MACROS_ASSERT_HEADER
 
 #    ifdef EXT_CHECKED
-#        define EXT_ASSERT(expr)           \
-            do {                           \
-                if (!(EXT_LIKELY(expr))) { \
-                    std::abort();          \
-                }                          \
+#        define EXT_ASSERT(expr)                                              \
+            do {                                                              \
+                if (!(EXT_LIKELY(expr))) {                                    \
+                    std::cerr << "ERROR: EXT_ASSERT(" #expr ")" << std::endl; \
+                    std::abort();                                             \
+                }                                                             \
             } while (0)
 #    else
 #        define EXT_ASSERT(expr) \
