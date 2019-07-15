b127d68b8c1ad1a08c88f3e76d12b23b51f7a6f5
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-07-15 17:34:27 +0200

    add modified knapsack version

--- include/ext/algorithm/knapsack.hpp
+++ include/ext/algorithm/knapsack.hpp
@@ -8,8 +8,8 @@
 #    include <stdexcept>
 #    include <type_traits>
 
+#    include <ext/macros/assert.hpp>
 #    include <ext/math/math_types.hpp>
-#include <iostream>
 
 namespace ext {
 namespace algorithm {
@@ -28,20 +28,20 @@ struct item {
 
 
 template<typename T>
-std::vector<item<T>> zero_or_one_knapsack(std::vector<item<T>>&& input, std::size_t max_weight) {
+inline std::vector<item<T>> zero_or_one_knapsack(std::vector<item<T>> const& input, std::size_t max_weight) {
 
     ext::math::dynamic_matrix<std::size_t> matrix(input.size() + 1, max_weight + 1);
     matrix.init_all_to(0);
 
     // m[i,w] = m[i-1,w] if w > w_i else m[ i, w - w_i] + v_i
     for (std::size_t i = 1; i < input.size() + 1; i++) {
-        auto const& item = input[i - 1]; // acess in input
-        for (std::size_t j = 1; j <= max_weight; j++) {
-            auto best_without_item = matrix.get(i - 1, j);
-            if (item.weight <= j) {
-                matrix.set(i, j, std::max(best_without_item, matrix.get(i - 1, j - item.weight) + item.value));
+        auto const& item = input[i - 1]; // access in input
+        for (std::size_t w = 1; w <= max_weight; w++) {
+            auto best_without_item = matrix.get(i - 1, w);
+            if (item.weight <= w) {
+                matrix.set(i, w, std::max(best_without_item, matrix.get(i - 1, w - item.weight) + item.value));
             } else {
-                matrix.set(i, j, best_without_item);
+                matrix.set(i, w, best_without_item);
             }
         }
     }
@@ -53,13 +53,13 @@ std::vector<item<T>> zero_or_one_knapsack(std::vector<item<T>>&& input, std::siz
     for (std::size_t i = input.size(); i > 0; i--) {
         auto const& item = input[i - 1]; // access in input
 
-        decltype(matrix)::value_type pick = 0;
+        decltype(matrix)::value_type if_picked_value = 0;
         if (rest_weight >= item.weight) { // can only be picked if capacity fits
-            pick = matrix.get(i - 1, rest_weight - item.weight) + item.value;
+            if_picked_value = matrix.get(i - 1, rest_weight - item.weight) + item.value;
         }
-        auto dont_pick = matrix.get(i - 1, rest_weight);
+        auto if_not_picked_value = matrix.get(i - 1, rest_weight);
 
-        if (pick > dont_pick) {
+        if (if_picked_value > if_not_picked_value) {
             rest_weight -= item.weight;
             output.push_back(item);
         }
@@ -68,14 +68,56 @@ std::vector<item<T>> zero_or_one_knapsack(std::vector<item<T>>&& input, std::siz
     return output;
 }
 
+template<typename T>
+inline std::vector<item<T>> zero_or_one_knapsack2(std::vector<item<T>> const& input, std::size_t max_weight) {
+    // matrix uses smaller type - but we need an extra array
+    // TODO - implement with bitfield matrix?
+    ext::math::dynamic_matrix<std::uint8_t> matrix(input.size() + 1, max_weight + 1);
+    matrix.init_all_to(0);
+
+    std::vector<std::size_t> max_for_weight(max_weight + 1);
+    std::fill(max_for_weight.begin(), max_for_weight.end(), 0);
+
+    for (std::uint64_t i = 1; i < input.size() + 1; i++) {
+        auto const& item = input[i - 1];
+        for (std::size_t w = max_weight; w > 0; w--) {
+            EXT_ASSERT(0 <= w && w <= max_weight);
+
+            std::size_t s = 0;
+            if (w >= item.weight) {
+                s = max_for_weight[w - item.weight] + item.value;
+            }
+
+            if (s > max_for_weight[w]) {
+                max_for_weight[w] = s;
+                matrix.set(i, w, 1);
+            } else {
+                matrix.set(i, w, 0);
+            }
+        }
+    }
+
+    std::vector<item<T>> output;
+    std::size_t rest_weight = max_weight;
+
+    for (std::size_t i = input.size(); i > 0; i--) {
+        auto const& item = input[i - 1]; // access in input
+        bool was_picked = matrix.get(i, rest_weight);
+        if (was_picked) {
+            rest_weight -= item.weight;
+            output.push_back(item);
+        }
+    }
+    return output;
+}
+
 } // namespace algorithm
 namespace util {
 
 template<typename T>
-std::ostream& operator<<(std::ostream& os, ext::algorithm::item<T> const& item) {
+inline std::ostream& operator<<(std::ostream& os, ext::algorithm::item<T> const& item) {
     return os << item.weight << item.value;
 }
-
 
 } // namespace util
 } // namespace ext
--- tests/algorithm_knapsack.cpp
+++ tests/algorithm_knapsack.cpp
@@ -5,19 +5,36 @@
 #include <ext/algorithm/knapsack.hpp>
 #include <ext/math/math_types.hpp>
 
+std::vector<ext::algorithm::item<void>> const input1{
+    {2, 1, nullptr},
+    {3, 2, nullptr},
+    {4, 5, nullptr},
+    {5, 6, nullptr},
+};
+std::size_t capacity1 = 8;
+std::size_t solution1 = 8;
 
-TEST(knapsack, zero_or_one) {
-    std::vector<ext::algorithm::item<void>> input{
-        {2, 1, nullptr},
-        {3, 2, nullptr},
-        {4, 5, nullptr},
-        {5, 6, nullptr},
-    };
+std::vector<ext::algorithm::item<void>> const input2{
+    {3, 4, nullptr}, // weight, value
+    {4, 6, nullptr},
+    {2, 2, nullptr},
+    {1, 5, nullptr},
+};
+std::size_t capacity2 = 6;
+std::size_t solution2 = 11;
 
-    auto out = ext::algorithm::zero_or_one_knapsack(std::move(input), 8);
-    auto result = std::accumulate(out.begin(), out.end(), 0UL, [](auto const& accumulation , auto const& item) {
+template<typename T>
+std::size_t accumulate(T const& container) {
+    return std::accumulate(container.begin(), container.end(), 0UL, [](auto const& accumulation, auto const& item) {
         return accumulation + item.value;
     });
+}
 
-    ASSERT_EQ(result, 8);
+
+//#    include <ext/util/scoped_timer.hpp>
+TEST(knapsack, zero_or_one) {
+    ASSERT_EQ(accumulate(ext::algorithm::zero_or_one_knapsack(input1, capacity1)), solution1);
+    ASSERT_EQ(accumulate(ext::algorithm::zero_or_one_knapsack(input2, capacity2)), solution2);
+    ASSERT_EQ(accumulate(ext::algorithm::zero_or_one_knapsack2(input1, capacity1)), solution1);
+    ASSERT_EQ(accumulate(ext::algorithm::zero_or_one_knapsack2(input2, capacity2)), solution2);
 }
