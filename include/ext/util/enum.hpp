1ce5746c4955d3d99624a98508609b93d86addb0
Michael T. Becker <programming@wickedmic.com>
2019-09-30 23:18:21 +0200

    add mics flag_set

--- include/ext/util/enum.hpp
+++ include/ext/util/enum.hpp
@@ -1,4 +1,5 @@
 // Copyright - 2019 - Jan Christoph Uhde <Jan@UhdeJC.com>
+// Copyright - 2019 - Michael T. Becker
 
 #ifndef EXT_UTIL_ENUM_HEADER
 #define EXT_UTIL_ENUM_HEADER
@@ -111,6 +112,135 @@ EnumType& operator&=(EnumType& e1, EnumType const& e2) {
 }
 
 } // namespace operators::enumeration
+
+
+// flag_set by Michael Becker
+// motivated by http://blog.bitwigglers.org/using-enum-classes-as-type-safe-bitmasks/
+template<typename T>
+struct flag_set {
+    using underlying_type = std::underlying_type_t<T>;
+
+    flag_set() : flags(0) {}
+
+    flag_set(T f) : flags(static_cast<underlying_type>(f)) {}
+
+    explicit flag_set(underlying_type flags) : flags(flags) {}
+
+    bool operator()(T f) const {
+        return (flags & static_cast<underlying_type>(f)) > 0;
+    }
+
+    flag_set& operator|=(flag_set other) {
+        flags |= other.flags;
+        return *this;
+    }
+
+    flag_set& operator&=(flag_set other) {
+        flags &= other.flags;
+        return *this;
+    }
+
+    flag_set& operator^=(flag_set other) {
+        flags ^= other.flags;
+        return *this;
+    }
+
+    flag_set& operator|=(T f) {
+        flags |= static_cast<underlying_type>(f);
+        return *this;
+    }
+
+    flag_set& operator&=(T f) {
+        flags &= static_cast<underlying_type>(f);
+        return *this;
+    }
+
+    flag_set& operator^=(T f) {
+        flags ^= static_cast<underlying_type>(f);
+        return *this;
+    }
+
+    underlying_type flags;
+};
+
+template<typename>
+struct is_flags_enum : std::false_type {};
+
+namespace operators::flag_set {
+using ::ext::util::flag_set;
+
+// --- flag_set operators ---
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator~(flag_set<T> fs) {
+    return ~fs.flags;
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator|(flag_set<T> rhs, flag_set<T> lhs) {
+    return rhs.flags | lhs.flags;
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator&(flag_set<T> rhs, flag_set<T> lhs) {
+    return rhs.flags & lhs.flags;
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator^(flag_set<T> rhs, flag_set<T> lhs) {
+    return rhs.flags ^ lhs.flags;
+}
+
+// --- enum operators ---
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator~(T f) {
+    return ~flag_set<T>(f);
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator|(T rhs, T lhs) {
+    return flag_set<T>(rhs) | flag_set<T>(lhs);
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator|(flag_set<T> rhs, T lhs) {
+    return rhs | flag_set<T>(lhs);
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator|(T rhs, flag_set<T> lhs) {
+    return flag_set<T>(rhs) | lhs;
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator&(T rhs, T lhs) {
+    return flag_set<T>(rhs) & flag_set<T>(lhs);
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator&(flag_set<T> rhs, T lhs) {
+    return rhs & flag_set<T>(lhs);
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator&(T rhs, flag_set<T> lhs) {
+    return flag_set<T>(rhs) & lhs;
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator^(T rhs, T lhs) {
+    return flag_set<T>(rhs) ^ flag_set<T>(lhs);
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator^(flag_set<T> rhs, T lhs) {
+    return rhs ^ flag_set<T>(lhs);
+}
+
+template<typename T, typename = std::enable_if_t<is_flags_enum<T>::value>>
+flag_set<T> operator^(T rhs, flag_set<T> lhs) {
+    return flag_set<T>(rhs) ^ lhs;
+}
+} // namespace operators::flag_set
 
 }}     // namespace ext::util
 #endif // EXT_UTIL_ENUM_HEADER
