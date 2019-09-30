c838f42a0b2d76dea7ef1b00f654261041fb0024
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-09-30 15:13:43 +0200

    move enum casts from cast.hpp to enum.hpp

--- include/ext/util/enum.hpp
+++ include/ext/util/enum.hpp
@@ -62,5 +62,51 @@ EnumType& operator&=(EnumType& e1, EnumType const& e2) {
     e1 = e1 & e2;
     return e1;
 }
+
+
+namespace _detail {
+// use underlying type if Type is a enum
+template<typename E, typename U = std::underlying_type_t<E>>
+using enable_if_enum_t = std::enable_if_t<std::is_enum_v<E>, U>;
+
+template<typename E1, typename E2>
+constexpr bool is_enums_v = std::is_enum_v<E1>&& std::is_enum_v<E2>;
+
+template<typename E1, typename E2>
+constexpr bool is_same_underlying_v = std::is_same_v<std::underlying_type_t<E1>, std::underlying_type_t<E1>>;
+} // namespace _detail
+
+/// to underlying
+template<typename T, typename Enum, typename U = std::underlying_type_t<Enum>>
+inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<T, U>, T> enum_to_underlying(Enum e) noexcept {
+    return static_cast<U>(e);
+}
+
+template<typename Enum, typename U = std::underlying_type_t<Enum>>
+inline constexpr _detail::enable_if_enum_t<Enum, U> enum_to_underlying_unsafe(Enum e) noexcept {
+    return static_cast<U>(e);
+}
+
+/// to enum
+template<typename Enum, typename T, typename U = std::underlying_type_t<Enum>>
+inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<U, T>, Enum>
+    underlying_to_enum(T value) noexcept {
+    return static_cast<Enum>(value);
+}
+
+/// enum to enum
+template<typename EnumOut, typename EnumIn>
+inline constexpr std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut>, EnumOut>
+    enum_to_enum_unsafe(EnumIn e) noexcept {
+    return underlying_to_enum<EnumOut>(enum_to_underlying_unsafe(e));
+}
+
+template<typename EnumOut, typename EnumIn>
+inline constexpr std::
+    enable_if_t<_detail::is_enums_v<EnumIn, EnumOut> && _detail::is_same_underlying_v<EnumIn, EnumOut>, EnumOut>
+    enum_to_enum(EnumIn e) noexcept {
+    return underlying_to_enum<EnumOut>(enum_to_underlying_unsafe(e));
+}
+
 }}     // namespace ext::util
 #endif // EXT_UTIL_ENUM_HEADER
--- tests/util_enum.cpp
+++ tests/util_enum.cpp
@@ -55,3 +55,30 @@ TEST(util_enum, and) {
         ASSERT_EQ(testenum::one, x);
     }
 }
+
+TEST(util_enum, enum_to_underlying) {
+    enum class enum_a : int { one = 1, two = 2, three = 3 };
+
+    ASSERT_EQ(eu::enum_to_underlying_unsafe(enum_a::one), 1);
+    ASSERT_EQ(eu::enum_to_underlying<int>(enum_a::one), 1);
+}
+
+TEST(util_enum, underlying_to_enum) {
+    enum class enum_a : int { one = 1, two = 2, three = 3 };
+
+    enum class enum_b : long { one = 1, two = 2, three = 3 };
+
+    ASSERT_EQ(eu::underlying_to_enum<enum_a>(2), enum_a::two);
+    ASSERT_EQ(eu::underlying_to_enum<enum_b>(2L), enum_b::two);
+}
+
+TEST(util_enum, enum_to_enum) {
+    enum class enum_a : int { one = 1, two = 2, three = 3 };
+
+    enum class enum_b : int { one = 1, two = 2, three = 3 };
+
+    enum class enum_c : long { one = 1, two = 2, three = 3 };
+
+    ASSERT_EQ(eu::enum_to_enum<enum_b>(enum_a::three), enum_b::three);
+}
+
