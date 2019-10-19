fb631e8f1738dc993eb8cd156286ab5cbe0da142
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-19 21:38:38 +0200

    modularize clang-format scripts

--- support/clang-format-all.py
+++ support/clang-format-all.py
@@ -1,91 +1,24 @@
 #!/usr/bin/env python3
 #Copyright - 2019 - Jan Christoph Uhde <Jan@UhdeJC.com>
-
 import os, sys, re
 import logging as log
 
 from pathlib import Path
 from subprocess import Popen, PIPE, STDOUT
 
-def apply_actuion_to_files(path, action, *filters):
-    for root, dirs, files in os.walk(path.resolve()):
-        for filename in files:
-            file_path=Path(root, filename)
+python_dir = Path(__file__).parent.resolve()
+modules_dir = python_dir.joinpath("python_modules")
+sys.path.insert(0,str(modules_dir)) #add modules dir
 
-            allow = True
-            for filter in filters:
-                if not (filter(file_path)):
-                    allow = False
-                    break
-
-            if allow:
-                action(file_path.resolve())
-
-def filter_id(path):
-    return True
-
-def filter_path(*paths):
-    def filter(path: Path):
-        absolute_path = path.resolve()
-        log.debug("path to check: {}".format(str(absolute_path)))
-        for prefix in paths:
-            absolute_prefix_path = Path(prefix).resolve()
-            log.debug("path to check: {}".format(str(absolute_prefix_path)))
-            if str(absolute_path).startswith(str(absolute_prefix_path)):
-                return True
-        return False
-    return filter
-
-def filter_cpp(path):
-    if path.suffix in [ ".cpp", ".cc", ".c", ".hpp", ".h" ]:
-        return True
-    else:
-        return False
-
-def find_clang_format(version = None):
-    candidates = [
-        "clang-format-ext",
-        "clang-format-9",
-        "clang-format"
-    ]
-
-    for candidate in candidates:
-        candidate_path = Path(candidate)
-        log.debug("testing candidate {}".format(candidate_path))
-
-        phandle = None
-        try:
-            phandle = Popen(
-                [ candidate_path, '--version', ],
-                stdout = PIPE, stderr = STDOUT,
-                universal_newlines=True
-            )
-        except FileNotFoundError as e:
-            continue
-
-        version_re = re.compile("version {}".format(version))
-
-        stdout = []
-        while phandle.poll() == None:
-            out_err = phandle.communicate(timeout = 1)
-            stdout += [ out_err[0] ]
-
-        out=" ".join(stdout).replace('\n', ' ')
-
-        if phandle.returncode == 0:
-            log.debug(out)
-            match = version_re.search(out)
-            if match:
-                return candidate_path
-
-    return None
-
+from obi.util.path_helper import apply_action_to_files, filter_cpp, create_filter_path
+from codecheck.util import find_clang_format
+from codecheck import logger
 
 def create_action_format():
     clang_format = find_clang_format(9.0)
 
     if not clang_format:
-        log.fatal("no matching clang-format found")
+        logger.fatal("no matching clang-format found")
         sys.exit(1)
 
     def action(path: Path):
@@ -104,12 +37,12 @@ def create_action_format():
 path = Path(os.curdir)
 
 project_dir = Path(__file__).resolve().parent.parent
-apply_actuion_to_files(
+apply_action_to_files(
     project_dir,
     create_action_format(),
     filter_cpp,
-    filter_path(Path(project_dir, "include"),
-                Path(project_dir, "tests"),
-                Path(project_dir, "examples")
-               )
+    create_filter_path(Path(project_dir, "include"),
+                       Path(project_dir, "tests"),
+                       Path(project_dir, "examples")
+                      )
 )
--- support/python_modules/codecheck/__init__.py
+++ support/python_modules/codecheck/__init__.py
@@ -2,7 +2,7 @@
 from obi.util.logging_helper import create_logger
 from obi.util.logging_helper import add_obi_formatter
 from obi.util.logging_helper import add_obi_formatter_short
-logger = create_logger("check_modify_code")
+logger = create_logger(__name__)
 
 for handler in logger.handlers:
     add_obi_formatter_short(handler)
