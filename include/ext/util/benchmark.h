260a142ea0c06e27ca726295486405043f352ca0
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-19 21:52:08 +0200

    apply format

--- include/ext/util/benchmark.h
+++ include/ext/util/benchmark.h
@@ -1,10 +1,9 @@
 // Copyright - 2019 - Jan Christoph Uhde <Jan@UhdeJC.com>
-#pragma once
 #ifndef EXT_UTIL_BENCHMARK_HEADER
 #define EXT_UTIL_BENCHMARK_HEADER
 
-#include <ext/macros/compiler.hpp>
 #include <atomic>
+#include <ext/macros/compiler.hpp>
 
 namespace ext { namespace util {
 
@@ -12,9 +11,9 @@ inline void do_not_optimize(void* pointer) {
 #ifdef EXT_COMPILER_GNU
     // Ihttps://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#Extended-Asm
     asm volatile(""             // template
-                :               // output
-                : "g"(pointer)  // input - "g" any general register
-                : "memory");    // clobber - Note that this clobber does not
+                 :              // output
+                 : "g"(pointer) // input - "g" any general register
+                 : "memory");   // clobber - Note that this clobber does not
                                 // prevent the processor from doing speculative
                                 // reads past the asm statement. To prevent that
                                 // you need processor-specific fence instructions.
@@ -27,17 +26,17 @@ inline void do_not_optimize(void* pointer) {
 
 inline void clobber_memory() {
 #ifdef EXT_COMPILER_GNU
-    asm volatile(""             // template
-                :               // output
-                :
-                : "memory");    // clobber - Note that this clobber does not
-                                // prevent the processor from doing speculative
-                                // reads past the asm statement. To prevent that
-                                // you need processor-specific fence instructions.
+    asm volatile("" // template
+                 :  // output
+                 :
+                 : "memory"); // clobber - Note that this clobber does not
+                              // prevent the processor from doing speculative
+                              // reads past the asm statement. To prevent that
+                              // you need processor-specific fence instructions.
 #else
     std::atomic_thread_fence(std::memory_order_seq_cst);
 #endif // EXT_COMPILER_GNU
 }
 
-}}
+}}     // namespace ext::util
 #endif // EXT_UTIL_BENCHMARK_HEADER
