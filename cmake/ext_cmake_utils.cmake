db7384e84693eb61cc29c5c33954cf728661ea3f
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-12-08 20:35:55 +0100

    add install function

--- cmake/ext_cmake_utils.cmake
+++ cmake/ext_cmake_utils.cmake
@@ -115,3 +115,27 @@ macro(ext_install lib)
 
     include(CPack)
 endmacro(ext_install)
+
+
+function(ext_install_files from_dir to_dir files)
+	foreach (file ${files})
+	    get_filename_component(parent ${file} DIRECTORY)
+	    if(IS_DIRECTORY ${from_dir}/${file})
+		    ext_log("install dir ${from_dir}/${file} to ${to_dir}/${parent}")
+	        install(
+	            DIRECTORY
+	                ${from_dir}/${file}
+	            DESTINATION
+	                ${to_dir}/${parent}
+	          )
+	    else()
+		    ext_log("install file ${from_dir}/${file} to ${to_dir}/${parent}")
+	        install(
+	            FILES
+	                ${from_dir}/${file}
+	            DESTINATION
+	                ${to_dir}/${parent}
+	        )
+	    endif()
+	endforeach()
+endfunction(ext_install_files)
