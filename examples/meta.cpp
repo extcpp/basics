a9ebe23491d554721b6bde15c4e7195a3f09fdc4
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-12-24 01:45:28 +0100

    allow unfinised code to be used in examples

--- examples/meta.cpp
+++ examples/meta.cpp
@@ -17,7 +17,7 @@ struct unleet {};
 int main() {
     std::cout << "is_one_of<int,double,float,int>(): " << is_one_of<int, double, float, int>() << std::endl;
     std::cout << "is_one_of<int,double,float>():     " << is_one_of<int, double, float>() << std::endl;
-    std::cout << "is_one_of<void_t<int>,int,void>(): " << is_one_of<void_t<int>, int, void>() << std::endl;
+    std::cout << "is_one_of<void_t<int>,int,void>(): " << is_one_of<std::void_t<int>, int, void>() << std::endl;
     std::cout << "has_value_var<leet>():             " << has_var_value_v<leet> << std::endl;
     std::cout << "has_type_member<unleet>():         " << has_type_type_v<unleet> << std::endl;
 
