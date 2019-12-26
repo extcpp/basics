40d3d982e7758b60612dd03705d9fa7357e8db95
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-12-26 12:26:11 +0100

    re-enable example

--- examples/binary_index_tree.cpp
+++ examples/binary_index_tree.cpp
@@ -1,17 +1,18 @@
 // Copyright - 2016 - Jan Christoph Uhde <Jan@UhdeJC.com>
 #include <array>
-#include <iostream>
 #include <vector>
+#include <iostream>
+#include <iomanip>
 
 #include <ext/structures/binary_index_tree.hpp>
 
 int main(/*int argc, const char *argv[]*/) {
     std::size_t inc = 1;
     std::size_t dec = 31;
-    for (std::size_t i = 0; i < 6; i++) {
+    for (std::size_t i = 0; i < 20; i++) {
         std::cout << i << ": " << inc << " " << dec << std::endl;
-        ext::structures::detail::increase_lsb(inc);
-        ext::structures::detail::remove_lsb(dec);
+        inc = ext::structures::detail::parent_update(inc);
+        dec = ext::structures::detail::parent_interrogation(dec);
     }
 
     std::array<long, 16> arr{};
@@ -28,35 +29,35 @@ int main(/*int argc, const char *argv[]*/) {
     // index
     std::cout << "idx:      ";
     for (std::size_t j = 0; j < arr.size(); ++j) {
-        std::cout << j << "\t";
+        std::cout << std::setw(5) << j;
     }
     std::cout << std::endl;
 
     // input
     std::cout << "input:    ";
     for (auto& i : input) {
-        std::cout << i << "\t";
+        std::cout << std::setw(5) << i;
     }
     std::cout << std::endl;
 
     // get
     std::cout << "get:      ";
     for (std::size_t j = 0; j < arr.size(); ++j) {
-        std::cout << ext::structures::bit_get<long>(begin, end, j) << "\t";
+        std::cout << std::setw(5) << ext::structures::bit_get<long>(begin, end, j);
     }
     std::cout << std::endl;
 
     // cul
     std::cout << "cul:      ";
     for (std::size_t j = 0; j < arr.size(); ++j) {
-        std::cout << ext::structures::bit_get_sum<long>(begin, end, j) << "\t";
+        std::cout << std::setw(5) << ext::structures::bit_get_sum<long>(begin, end, j);
     }
     std::cout << std::endl;
 
     // stored
     std::cout << "store:    ";
     for (auto& i : arr) {
-        std::cout << i << "\t";
+        std::cout << std::setw(5) << i;
     }
     std::cout << std::endl;
 
