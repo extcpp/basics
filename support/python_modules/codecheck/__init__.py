a0d8579658e6e18c2c571cd7d36c0b6fa26a3cce
Jan Christoph Uhde <ObiWahn@users.noreply.github.com>
2020-01-31 13:14:02 +0100

    Remove build and support tools from source-tree (#25)

--- support/python_modules/codecheck/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from pathlib import Path
-import obi.util.logging_helper as lh
-
-logger = lh.init_logging(Path(__name__).stem)
-for handler in logger.handlers:
-    lh.add_obi_formatter_short(handler)
