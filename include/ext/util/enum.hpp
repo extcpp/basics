99f93521a5236db3e2be4217e3f42284ba327a5c
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-09-05 10:45:08 +0200

    Fix enum tests.

--- include/ext/util/enum.hpp
+++ include/ext/util/enum.hpp
@@ -15,7 +15,7 @@ template<typename E1, typename E2>
 constexpr bool is_enums_v = std::is_enum_v<E1>&& std::is_enum_v<E2>;
 
 template<typename E1, typename E2>
-constexpr bool is_same_underlying_v = std::is_same_v<std::underlying_type_t<E1>, std::underlying_type_t<E1>>;
+constexpr bool is_same_underlying_v = std::is_same_v<std::underlying_type_t<E1>, std::underlying_type_t<E2>>;
 
 template <typename E, typename = E>
 inline constexpr bool is_fixed_enum = false;
@@ -32,97 +32,52 @@ template <typename E>
 struct is_fixed_enum : std::bool_constant<is_fixed_enum_v<E>> {};
 
 /// to underlying
-template<typename T, typename Enum, typename U = std::underlying_type_t<Enum>>
-inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<T, U>, T> enum_to_underlying(Enum e) noexcept {
+template<typename T, typename Enum
+        ,typename U = std::underlying_type_t<Enum>
+        ,typename = std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<T, U>>
+        >
+inline constexpr T enum_to_underlying_safe(Enum e) noexcept {
     return static_cast<U>(e);
 }
 
-template<typename Enum, typename U = std::underlying_type_t<Enum>>
-inline constexpr _detail::enable_if_enum_t<Enum, U> enum_to_underlying_unsafe(Enum e) noexcept {
+template<typename Enum
+        ,typename U = std::underlying_type_t<Enum>
+        ,typename = _detail::enable_if_enum_t<Enum>
+        >
+inline constexpr U enum_to_underlying(Enum e) noexcept {
     return static_cast<U>(e);
 }
 
 /// to enum
-template<typename Enum, typename T, typename U = std::underlying_type_t<Enum>>
-inline constexpr std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<U, T>, Enum>
-    underlying_to_enum(T value) noexcept {
+template<typename Enum, typename T
+        ,typename U = std::underlying_type_t<Enum>
+        ,typename = std::enable_if_t<std::is_enum_v<Enum> && std::is_same_v<U, T>>
+        >
+inline constexpr Enum underlying_to_enum(T value) noexcept {
     return static_cast<Enum>(value);
 }
 
-/// enum to enum
-template<typename EnumOut, typename EnumIn>
-inline constexpr std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut>, EnumOut>
-    enum_to_enum_unsafe(EnumIn e) noexcept {
-    return underlying_to_enum<EnumOut>(enum_to_underlying_unsafe(e));
+template<typename Enum, typename T
+        ,typename = std::enable_if_t<std::is_enum_v<Enum> && std::is_integral_v<T>>
+        >
+inline constexpr Enum underlying_to_enum_unsafe(T value) noexcept {
+    return static_cast<Enum>(value);
 }
 
-template<typename EnumOut, typename EnumIn>
-inline constexpr std::
-    enable_if_t<_detail::is_enums_v<EnumIn, EnumOut> && _detail::is_same_underlying_v<EnumIn, EnumOut>, EnumOut>
-    enum_to_enum(EnumIn e) noexcept {
-    return underlying_to_enum<EnumOut>(enum_to_underlying_unsafe(e));
+/// enum to enum - one should be a subset of the other
+template<typename EnumOut, typename EnumIn
+        ,typename = std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut> && _detail::is_same_underlying_v<EnumIn, EnumOut>>
+        >
+inline constexpr EnumOut enum_to_enum(EnumIn e) noexcept {
+    return underlying_to_enum_unsafe<EnumOut>(enum_to_underlying(e));
 }
 
-
-namespace operators::enumeration {
-
-template<typename EnumType, typename IntegerType>
-EnumType operator|(EnumType const& e, IntegerType const& i) {
-    static_assert(std::is_enum_v<EnumType>);
-    static_assert(std::is_same_v<std::underlying_type_t<EnumType>, IntegerType>);
-    return static_cast<EnumType>(static_cast<IntegerType>(e) | i);
+template<typename EnumOut, typename EnumIn
+        ,typename = std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut>>
+        >
+inline constexpr EnumOut enum_to_enum_unsafe(EnumIn e) noexcept {
+    return underlying_to_enum_unsafe<EnumOut>(enum_to_underlying(e));
 }
-
-template<typename EnumType, typename IntegerType>
-EnumType& operator|=(EnumType& e, IntegerType const& i) {
-    static_assert(std::is_enum_v<EnumType>);
-    e = e | i;
-    return e;
-}
-
-template<typename EnumType>
-EnumType operator|(EnumType const& e1, EnumType const& e2) {
-    static_assert(std::is_enum_v<EnumType>);
-    using type = std::underlying_type_t<EnumType>;
-    return static_cast<EnumType>(static_cast<type>(e1) | static_cast<type>(e2));
-}
-
-template<typename EnumType>
-EnumType& operator|=(EnumType& e1, EnumType const& e2) {
-    static_assert(std::is_enum_v<EnumType>);
-    e1 = e1 | e2;
-    return e1;
-}
-
-template<typename EnumType, typename IntegerType>
-EnumType operator&(EnumType const& e, IntegerType const& i) {
-    static_assert(std::is_enum_v<EnumType>);
-    static_assert(std::is_same_v<std::underlying_type_t<EnumType>, IntegerType>);
-    return static_cast<EnumType>(static_cast<IntegerType>(e) & i);
-}
-
-template<typename EnumType, typename IntegerType>
-EnumType& operator&=(EnumType& e, IntegerType const& i) {
-    static_assert(std::is_enum_v<EnumType>);
-    e = e & i;
-    return e;
-}
-
-template<typename EnumType>
-EnumType operator&(EnumType const& e1, EnumType const& e2) {
-    static_assert(std::is_enum_v<EnumType>);
-    using type = std::underlying_type_t<EnumType>;
-    return static_cast<EnumType>(static_cast<type>(e1) & static_cast<type>(e2));
-}
-
-template<typename EnumType>
-EnumType& operator&=(EnumType& e1, EnumType const& e2) {
-    static_assert(std::is_enum_v<EnumType>);
-    e1 = e1 & e2;
-    return e1;
-}
-
-} // namespace operators::enumeration
 
 }}     // namespace ext::util
 #endif // EXT_UTIL_ENUM_HEADER
--- tests/util_enum.cpp
+++ tests/util_enum.cpp
@@ -5,11 +5,23 @@
 
 namespace eu = ::ext::util;
 
-TEST(util_enum, enum_to_underlying) {
-    enum class enum_a : int { one = 1, two = 2, three = 3 };
+TEST(util_enum, is_fixed_enum) {
+    enum class enum_a { };
+    enum class enum_b : uint8_t { };
+    enum enum_c { };
+    enum enum_d : uint8_t { };
 
-    ASSERT_EQ(eu::enum_to_underlying_unsafe(enum_a::one), 1);
-    ASSERT_EQ(eu::enum_to_underlying<int>(enum_a::one), 1);
+    static_assert(eu::is_fixed_enum_v<enum_a>);
+    static_assert(eu::is_fixed_enum_v<enum_b>);
+    static_assert(!eu::is_fixed_enum_v<enum_c>);
+    static_assert(eu::is_fixed_enum_v<enum_d>);
+}
+
+TEST(util_enum, enum_to_underlying) {
+    enum class enum_a : uint8_t { one = 1, two = 2, three = 3 };
+
+    ASSERT_EQ(eu::enum_to_underlying(enum_a::one), 1);
+    ASSERT_EQ(eu::enum_to_underlying_safe<uint8_t>(enum_a::one), 1);
 }
 
 TEST(util_enum, underlying_to_enum) {
@@ -18,6 +30,8 @@ TEST(util_enum, underlying_to_enum) {
 
     ASSERT_EQ(eu::underlying_to_enum<enum_a>(2), enum_a::two);
     ASSERT_EQ(eu::underlying_to_enum<enum_b>(2L), enum_b::two);
+
+    ASSERT_EQ(eu::underlying_to_enum_unsafe<enum_a>(2L), enum_a::two);
 }
 
 TEST(util_enum, enum_to_enum) {
@@ -25,71 +39,9 @@ TEST(util_enum, enum_to_enum) {
     enum class enum_b : int { one = 1, two = 2, three = 3 };
     enum class enum_c : long { one = 1, two = 2, three = 3 };
 
-    ASSERT_EQ(eu::enum_to_enum<enum_b>(enum_a::three), enum_b::three);
-}
+    // enums should be sub / supser sets of each other
+    ASSERT_EQ((eu::enum_to_enum<enum_b>(enum_a::three)), (enum_b::three));
 
-
-enum class testenum : int {
-    // clang-format off
-    one    = 0b0001,
-    two    = 0b0010,
-    three  = 0b0011,
-    four   = 0b0100,
-    five   = 0b0101,
-    six    = 0b0110,
-    seven  = 0b0111,
-    eight  = 0b1000,
-    nine   = 0b1001
-    // clang-format on
-};
-
-
-TEST(util_enum, or) {
-    using namespace eu::operators::enumeration;
-    ASSERT_EQ(testenum::three, testenum::one | testenum::two);
-    {
-        auto x = testenum::one;
-        x |= testenum::two;
-        ASSERT_EQ(testenum::three, x);
-    }
-
-    ASSERT_EQ(testenum::three, testenum::one | 2);
-    {
-        auto x = testenum::one;
-        x |= 2;
-        ASSERT_EQ(testenum::three, x);
-    }
-}
-
-TEST(util_enum, and) {
-    using namespace eu::operators::enumeration;
-    ASSERT_EQ(testenum::one, testenum::three & testenum::one);
-    {
-        auto x = testenum::three;
-        x &= testenum::one;
-        ASSERT_EQ(testenum::one, x);
-    }
-
-    ASSERT_EQ(testenum::one, testenum::three & 1);
-    {
-        auto x = testenum::three;
-        x &= 1;
-        ASSERT_EQ(testenum::one, x);
-    }
-}
-
-// TODO - add more tests
-EXT_ENABLE_FLAG_SET_OPERATORS(testenum)
-
-TEST(util_enum, flag_set) {
-    using namespace eu::operators::flag_set;
-    {
-        flag_set<testenum> x = (testenum::one | testenum::two);
-        ASSERT_EQ(testenum::three, eu::underlying_to_enum<testenum>(x.flags));
-    }
-
-    {
-        flag_set<testenum> x = (testenum::three & testenum::one);
-        ASSERT_EQ(testenum::one, eu::underlying_to_enum<testenum>(x.flags));
-    }
+    // enums are of same underlying types
+    ASSERT_EQ((eu::enum_to_enum_unsafe<enum_c>(enum_a::three)), (enum_c::three));
 }
