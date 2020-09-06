1baec549f3d6bf53686b4ccb777c19486d518000
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-09-06 14:56:19 +0200

    Use `void_t` to make expression SFINAE more explicit.

--- include/ext/util/enum.hpp
+++ include/ext/util/enum.hpp
@@ -4,29 +4,29 @@
 #define EXT_UTIL_ENUM_HEADER
 #include <type_traits>
 
-namespace ext { namespace util {
+namespace ext::util {
 
-namespace _detail {
-// use underlying type if Type is a enum
-template<typename E, typename U = std::underlying_type_t<E>>
-using enable_if_enum_t = std::enable_if_t<std::is_enum_v<E>, U>;
+namespace _detail_enum {
+    // use underlying type if Type is a enum
+    template<typename E, typename U = std::underlying_type_t<E>>
+    using enable_if_enum_t = std::enable_if_t<std::is_enum_v<E>, U>;
 
-template<typename E1, typename E2>
-constexpr bool is_enums_v = std::is_enum_v<E1>&& std::is_enum_v<E2>;
+    template<typename E1, typename E2>
+    constexpr bool are_enums_v = std::is_enum_v<E1>&& std::is_enum_v<E2>;
 
-template<typename E1, typename E2>
-constexpr bool is_same_underlying_v = std::is_same_v<std::underlying_type_t<E1>, std::underlying_type_t<E2>>;
+    template<typename E1, typename E2>
+    constexpr bool is_same_underlying_v = std::is_same_v<std::underlying_type_t<E1>, std::underlying_type_t<E2>>;
 
-template <typename E, typename = E>
-inline constexpr bool is_fixed_enum = false;
+    template <typename E, typename = void>
+    inline constexpr bool is_fixed_enum = false;
 
-template <typename E>
-inline constexpr bool is_fixed_enum<E, decltype(E{0})> = std::is_enum_v<E>;
-} // namespace _detail
+    template <typename E>
+    inline constexpr bool is_fixed_enum<E, std::void_t<decltype(E{0})>> = std::is_enum_v<E>;
+} // namespace _detail_enum
 
 /// check if enum is of fixed type
 template <typename E>
-inline constexpr bool is_fixed_enum_v = _detail::is_fixed_enum<E>;
+inline constexpr bool is_fixed_enum_v = _detail_enum::is_fixed_enum<E>;
 
 template <typename E>
 struct is_fixed_enum : std::bool_constant<is_fixed_enum_v<E>> {};
@@ -34,7 +34,7 @@ struct is_fixed_enum : std::bool_constant<is_fixed_enum_v<E>> {};
 /// to underlying
 template<typename Enum
         ,typename U = std::underlying_type_t<Enum>
-        ,typename = _detail::enable_if_enum_t<Enum>
+        ,typename = _detail_enum::enable_if_enum_t<Enum>
         >
 inline constexpr U enum_to_underlying(Enum e) noexcept {
     return static_cast<U>(e);
@@ -66,18 +66,19 @@ inline constexpr Enum underlying_to_enum_unsafe(T value) noexcept {
 
 /// enum to enum - one should be a subset of the other
 template<typename EnumOut, typename EnumIn
-        ,typename = std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut> && _detail::is_same_underlying_v<EnumIn, EnumOut>>
+        ,typename = std::enable_if_t<_detail_enum::are_enums_v<EnumIn, EnumOut> &&
+                                     _detail_enum::is_same_underlying_v<EnumIn, EnumOut>>
         >
 inline constexpr EnumOut enum_to_enum(EnumIn e) noexcept {
     return underlying_to_enum_unsafe<EnumOut>(enum_to_underlying(e));
 }
 
 template<typename EnumOut, typename EnumIn
-        ,typename = std::enable_if_t<_detail::is_enums_v<EnumIn, EnumOut>>
+        ,typename = std::enable_if_t<_detail_enum::are_enums_v<EnumIn, EnumOut>>
         >
 inline constexpr EnumOut enum_to_enum_unsafe(EnumIn e) noexcept {
     return underlying_to_enum_unsafe<EnumOut>(enum_to_underlying(e));
 }
 
-}}     // namespace ext::util
+}     // namespace ext::util
 #endif // EXT_UTIL_ENUM_HEADER
