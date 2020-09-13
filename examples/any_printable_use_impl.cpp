97fe655ae1b020cac4b65aa54809c2fa6173821e
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-09-13 17:48:44 +0200

    Remove any_printable.

--- examples/any_printable_use_impl.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright - 2020 - Jan Christoph Uhde <Jan@UhdeJC.com>
-// Please see LICENSE.md for license or visit https://github.com/extcpp/basics
-#include <ext/util/any_printable.hpp>
-using namespace ext::util;
-
-void use(any_printable const& any) {
-    std::cout << any << std::endl;
-}
