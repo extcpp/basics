dc44786179a4c8ad3b6e5ce8775c4eb02789a1c2
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-10-04 20:57:34 +0200

    Do not use reserved strings (underscore upper-case).

--- include/ext/macros/advanced.hpp
+++ include/ext/macros/advanced.hpp
@@ -9,13 +9,13 @@
 #define EXT_GET2ND(first, second, ...) second
 
 // returns 0 if there is only one arg. return 2 arg otherwise
-#define _EXT_ZERO_IF_ONE_ARG(...) EXT_GET2ND(__VA_ARGS__, 0, )
-#define _EXT_SET2ND_TO_ONE(first) first, 1
+#define EXT_INTERNAL_ZERO_IF_ONE_ARG(...) EXT_GET2ND(__VA_ARGS__, 0, )
+#define EXT_INTERNAL_SET2ND_TO_ONE(first) first, 1
 
 // Now we can test for the marker if it gets expanded
 // the 2nd arg should be set to 1 and that shall be returned
-#define EXT_TEST _EXT_ZERO_IF_ONE_ARG
-#define EXT_MARK _EXT_SET2ND_TO_ONE
+#define EXT_TEST EXT_INTERNAL_ZERO_IF_ONE_ARG
+#define EXT_MARK EXT_INTERNAL_SET2ND_TO_ONE
 // check for marker - end ///////////////////////////////////////////
 
 #endif // EXT_MACROS_ADVANCED_HEADER
