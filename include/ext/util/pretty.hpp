6b130cde032a2917a5b068281991b65fddc9e629
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-12-07 22:21:56 +0100

    Rename show namespace and header to pretty.
    The show funtions are renamed to fmt.

--- include/ext/util/pretty.hpp
+++ include/ext/util/pretty.hpp
@@ -1,7 +1,7 @@
 // Copyright - 2016-2020 - Jan Christoph Uhde <Jan@UhdeJC.com>
 // Please see LICENSE.md for license or visit https://github.com/extcpp/basics
-#ifndef EXT_UTIL_SHOW_HEADER
-#define EXT_UTIL_SHOW_HEADER
+#ifndef EXT_UTIL_PRETTY_HEADER
+#define EXT_UTIL_PRETTY_HEADER
 #include <iostream>
 #include <sstream>
 #include <tuple>
@@ -11,7 +11,7 @@
 
 namespace ext::util {
 
-namespace show {
+namespace pretty {
 // function to print containers
 template<typename T>
 inline std::enable_if_t<_detail::is_container<T>::value, std::ostream&> operator<<(std::ostream& out,
@@ -19,7 +19,7 @@ inline std::enable_if_t<_detail::is_container<T>::value, std::ostream&> operator
 }
 
 // functions that add quoting in containers
-namespace _detail::show_internal {
+namespace _detail::pretty {
 //string_view
 inline std::ostream& operator<<(std::ostream& out, std::string_view const& str) {
     using namespace std::literals::string_literals;
@@ -51,21 +51,21 @@ inline std::ostream& operator<<(std::ostream& out, char const* str) {
 template<typename Key, typename Value>
 inline std::ostream& out_pair_in_map(std::ostream& out, const std::pair<Key, Value>& pair) {
     using namespace std::literals::string_literals;
-    using ext::util::_detail::show_internal::operator<<;
-    using ext::util::show::operator<<; // it could be anohter container
+    using ext::util::_detail::pretty::operator<<;
+    using ext::util::pretty::operator<<; // it could be anohter container
     out << pair.first;
     std::operator<<(out, ":"s) << pair.second;
     return out;
 }
 
 template<typename Container, typename T>
-inline std::ostream& show(std::ostream& out, T&& value) {
-    using ext::util::_detail::show_internal::operator<<;
+inline std::ostream& fmt_internal(std::ostream& out, T&& value) {
+    using ext::util::_detail::pretty::operator<<;
     if constexpr (_detail::is_associative<Container>::value) {
         out_pair_in_map(out, std::forward<T>(value));
     } else {
-        using ext::util::_detail::show_internal::operator<<;
-        using ext::util::show::operator<<;
+        using ext::util::_detail::pretty::operator<<;
+        using ext::util::pretty::operator<<;
         out << std::forward<T>(value);
     }
     return out;
@@ -74,7 +74,7 @@ inline std::ostream& show(std::ostream& out, T&& value) {
 } // namespace _detail
 
 
-namespace show {
+namespace pretty {
 // diverse container
 template<typename T>
 inline std::enable_if_t<_detail::is_container<T>::value, std::ostream&> operator<<(std::ostream& out,
@@ -91,13 +91,13 @@ inline std::enable_if_t<_detail::is_container<T>::value, std::ostream&> operator
         auto current = container.begin();
         if (container.size() > 1) {
             while (next(current) != container.end()) {
-                using ext::util::_detail::show_internal::operator<<;
-                _detail::show_internal::show<T>(out, *current);
+                using ext::util::_detail::pretty::operator<<;
+                _detail::pretty::fmt_internal<T>(out, *current);
                 std::operator<<(out, ", "s);
                 current++;
             }
         }
-        _detail::show_internal::show<T>(out, *current);
+        _detail::pretty::fmt_internal<T>(out, *current);
     } else {
         std::operator<<(out, " "s);
     }
@@ -117,7 +117,7 @@ inline std::enable_if_t<_detail::is_container<T>::value, std::ostream&> operator
 template<typename Key, typename Value>
 inline std::ostream& operator<<(std::ostream& out, const std::pair<Key, Value>& pair) {
     using namespace std::literals::string_literals;
-    using ext::util::_detail::show_internal::operator<<;
+    using ext::util::_detail::pretty::operator<<;
     std::operator<<(out, "("s) << pair.first;
     std::operator<<(out, ", "s) << pair.second;
     std::operator<<(out, ")"s);
@@ -127,17 +127,17 @@ inline std::ostream& operator<<(std::ostream& out, const std::pair<Key, Value>& 
 
 // tuple
 template<typename Tuple, std::size_t... I>
-inline std::ostream& show(std::ostream& out, const Tuple& tuple, std::index_sequence<I...>) {
+inline std::ostream& fmt(std::ostream& out, const Tuple& tuple, std::index_sequence<I...>) {
     using namespace std::literals::string_literals;
 
     std::operator<<(out, "("s);
-    using ext::util::_detail::show_internal::operator<<;
+    using ext::util::_detail::pretty::operator<<;
     ((std::operator<<(out, (I == 0 ? ""s : ", "s)) << std::get<I>(tuple)), ...);
     std::operator<<(out, ")"s);
     return out;
 }
 
-inline std::ostream& show(std::ostream& out, const std::tuple<>&, std::index_sequence<>) {
+inline std::ostream& fmt(std::ostream& out, const std::tuple<>&, std::index_sequence<>) {
     using namespace std::literals::string_literals;
     std::operator<<(out, "( )"s);
     return out;
@@ -145,18 +145,18 @@ inline std::ostream& show(std::ostream& out, const std::tuple<>&, std::index_seq
 
 template<typename... T>
 inline constexpr std::ostream& operator<<(std::ostream& out, const std::tuple<T...>& tuple) {
-    return show(out, tuple, std::make_index_sequence<sizeof...(T)>());
+    return fmt(out, tuple, std::make_index_sequence<sizeof...(T)>());
 }
 // tuple - end
 
 template<typename T>
-inline std::string show(T&& item) {
+inline std::string fmt(T&& item) {
     std::stringstream ss;
     ss << std::boolalpha << std::forward<T>(item);
     return ss.str();
 }
 
-} // namespace show - end
+} // namespace pretty - end
 
 }     // namespace ext::util
-#endif // EXT_UTIL_SHOW_HEADER
+#endif // EXT_UTIL_PRETTY_HEADER
--- tests/util_show.cpp
+++ tests/util_show.cpp
@@ -1,65 +1,65 @@
 // Copyright - 2020 - Jan Christoph Uhde <Jan@UhdeJC.com>
 // Please see LICENSE.md for license or visit https://github.com/extcpp/basics
-#include <ext/util/show.hpp>
+#include <ext/util/pretty.hpp>
 #include <gtest/gtest.h>
 using namespace std::literals::string_literals;
 
-namespace eu = ::ext::util::show;
+namespace eu = ::ext::util::pretty;
 
-TEST(util_show, string) {
-    ASSERT_EQ(eu::show("foo"s), "foo");
+TEST(util_pretty, string) {
+    ASSERT_EQ(eu::fmt("foo"s), "foo");
 }
 
-TEST(util_show, string_view) {
+TEST(util_pretty, string_view) {
     auto out = "bar"s;
-    ASSERT_EQ(eu::show(std::string_view(out)), "bar");
+    ASSERT_EQ(eu::fmt(std::string_view(out)), "bar");
 }
 
-TEST(util_show, vec_empty) {
-    ASSERT_EQ(eu::show(std::vector<int>{}), "[ ]");
+TEST(util_pretty, vec_empty) {
+    ASSERT_EQ(eu::fmt(std::vector<int>{}), "[ ]");
 }
 
-TEST(util_show, vec_one_item) {
-    ASSERT_EQ(eu::show(std::vector{1}), "[1]");
+TEST(util_pretty, vec_one_item) {
+    ASSERT_EQ(eu::fmt(std::vector{1}), "[1]");
 }
 
-TEST(util_show, vec_int) {
-    ASSERT_EQ(eu::show(std::vector{1, 2, 3, 4, 5, 6}), "[1, 2, 3, 4, 5, 6]");
+TEST(util_pretty, vec_int) {
+    ASSERT_EQ(eu::fmt(std::vector{1, 2, 3, 4, 5, 6}), "[1, 2, 3, 4, 5, 6]");
 }
 
-TEST(util_show, vec_string) {
-    ASSERT_EQ(eu::show(std::vector<std::string>{"foo", "bar"}), "[\"foo\", \"bar\"]");
+TEST(util_pretty, vec_string) {
+    ASSERT_EQ(eu::fmt(std::vector<std::string>{"foo", "bar"}), "[\"foo\", \"bar\"]");
 }
 
-TEST(util_show, pair) {
-    ASSERT_EQ(eu::show(std::pair{"foo"s, 23}), "(\"foo\", 23)");
+TEST(util_pretty, pair) {
+    ASSERT_EQ(eu::fmt(std::pair{"foo"s, 23}), "(\"foo\", 23)");
 }
 
-TEST(util_show, map_int_str) {
+TEST(util_pretty, map_int_str) {
     using Map = std::map<int, std::string>;
     Map out{{2, "foo"s}, {3, "bar"s}};
-    ASSERT_EQ(eu::show(out), "{2:\"foo\", 3:\"bar\"}");
+    ASSERT_EQ(eu::fmt(out), "{2:\"foo\", 3:\"bar\"}");
 }
 
-TEST(util_show, vec_map_vec_int_str) {
+TEST(util_pretty, vec_map_vec_int_str) {
     using Map = std::map<std::vector<int>, std::string>;
     Map item{{std::vector{1, 2}, "foo"s}, {std::vector{3}, "bar"s}};
     std::vector<Map> out = {item, item};
-    ASSERT_EQ(eu::show(out), "[{[1, 2]:\"foo\", [3]:\"bar\"}, {[1, 2]:\"foo\", [3]:\"bar\"}]");
+    ASSERT_EQ(eu::fmt(out), "[{[1, 2]:\"foo\", [3]:\"bar\"}, {[1, 2]:\"foo\", [3]:\"bar\"}]");
 }
 
-TEST(util_show, set_int) {
-    ASSERT_EQ(eu::show(std::set{1, 2, 3}), "{1, 2, 3}");
+TEST(util_pretty, set_int) {
+    ASSERT_EQ(eu::fmt(std::set{1, 2, 3}), "{1, 2, 3}");
 }
 
-TEST(util_show, tuple) {
-    ASSERT_EQ(eu::show(std::tuple<>{}), "( )");
-    ASSERT_EQ(eu::show(std::tuple<int, int, int, std::string>{3, 2, 1, "risiko"}), "(3, 2, 1, \"risiko\")");
-    ASSERT_EQ(eu::show(std::tuple<std::string>{"DarkwingDuck"}), "(\"DarkwingDuck\")");
+TEST(util_pretty, tuple) {
+    ASSERT_EQ(eu::fmt(std::tuple<>{}), "( )");
+    ASSERT_EQ(eu::fmt(std::tuple<int, int, int, std::string>{3, 2, 1, "risiko"}), "(3, 2, 1, \"risiko\")");
+    ASSERT_EQ(eu::fmt(std::tuple<std::string>{"DarkwingDuck"}), "(\"DarkwingDuck\")");
 }
 
-TEST(util_show, vec_bool) {
+TEST(util_pretty, vec_bool) {
     // vector<bool> - seriously? ==> https://github.com/vector-of-bool/dds
     using namespace std;
-    ASSERT_EQ(eu::show(vector<bool>{true, true, false}), "[true, true, false]");
+    ASSERT_EQ(eu::fmt(vector<bool>{true, true, false}), "[true, true, false]");
 }
