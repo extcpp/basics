abca8f02dc1c8ef56ca0aa7eb386c40fac4dbf27
Jan Christoph Uhde <Jan@UhdeJC.com>
2021-10-06 23:52:10 +0200

    add foreach in tuple

---
+++ tests/util_tuple.cpp
@@ -0,0 +1,19 @@
+// Copyright - 2020 - Jan Christoph Uhde <Jan@UhdeJC.com>
+// Please see LICENSE.md for license or visit https://github.com/extcpp/basics
+#include <ext/macros/compiler.hpp>
+#if __cplusplus <= 201703L
+    EXT_WARNING("skipping build of tuple test because of old compiler")
+#else
+
+#include <ext/util/tuple.hpp>
+#include <gtest/gtest.h>
+#include <string>
+
+TEST(util_tuple, tuple_for_each) {
+    std::tuple<int,int,int> tup(1,2,3);
+    std::vector<int> result;
+    ext::util::tuple_for_each(tup,[&result](int x){ result.push_back(x); });
+    ASSERT_EQ(result, (std::vector<int>{1, 2, 3}));
+}
+
+#endif
