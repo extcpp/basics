4b3f590ebd7335d3dec827e8c7bd08c768cfb230
Jan Christoph Uhde <Jan@UhdeJC.com>
2023-03-22 23:21:06 +0100

    add: template_string_literal class

---
+++ include/ext/util/template_string_literal.hpp
@@ -0,0 +1,19 @@
+// Copyright - Jan Christoph Uhde <Jan@UhdeJC.com>
+// Please see LICENSE.md for license or visit https://github.com/extcpp/basics
+#ifndef EXT_UTIL_TEMPLATE_STRING_LITERAL_HEADER
+#define EXT_UTIL_TEMPLATE_STRING_LITERAL_HEADER
+#include <algorithm>
+
+namespace ext::util {
+
+// c++20 -- class types in non-type template parameters
+template<std::size_t N>
+struct template_string_literal {
+    constexpr template_string_literal(const char (&str)[N]) {
+        std::copy_n(str, N, value);
+    }
+    char value[N];
+};
+
+} // namespace ext::util
+#endif
